c4cb9128f48119f97ef2276e8997d740cc14c277
Jat97 <85377239+Jat97@users.noreply.github.com>
2025-08-03 02:12:50 -0400

    Edited createCommentTree function to include likes for each comment

--- express-bookmark/database/misc.js
+++ express-bookmark/database/misc.js
@@ -6,19 +6,29 @@ module.exports.findGroup = async (req) => {
     return found_group.rows[0];
 };
 
-module.exports.createCommentTree = (comments) => {
+module.exports.createCommentTree = async (comments) => {
     const nodes = {};
 
     const tree = [];
 
     comments.forEach(comment => {
         if(comment.reply_to === null) {
-            nodes[comment.id] = {...comment, replies: []}
+            nodes[comment.id] = {...comment, likes: [], replies: []}
         }
     });
 
-    comments.forEach(comment => {
+    for(const comment of comments) {
         const new_node = nodes[comment.id];
+
+        const comment_likes = await db.query(`SELECT id as id,
+            users.liking_user as liking_user,
+            comments.liked_comment as liked_comment
+            FROM likes
+            WHERE liked_comment = $1`, 
+            [comment.id]
+        );
+
+        nodes[comment.id].likes.concat(comment_likes.rows);
 
         if(comment.reply_to !== null) {
             nodes[comment.id].replies.push(comment);
@@ -26,7 +36,7 @@ module.exports.createCommentTree = (comments) => {
         else {
             tree.push(new_node);
         }
-    });
+    }
 
     return tree;
 }
