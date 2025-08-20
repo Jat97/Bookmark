db9d05a58e1cfc9c700a843b984282e84bad69e2
Jat97 <85377239+Jat97@users.noreply.github.com>
2025-08-20 02:11:04 -0400

    Added boxes for members and requests, including links for full view

---
+++ vite-bookmark/src/Routes/Groups/Group.jsx
@@ -0,0 +1,97 @@
+import {Link, useParams} from 'react-router-dom';
+import {PencilIcon} from '@heroicons/react/24/solid';
+import {useFetchLogged} from '../Functions/Queries/UserQueries';
+import {useFetchGroups} from '../Functions/Queries/GroupQueries';
+import {bookStore} from '../../Context/bookStore';
+import GroupDisplay from './GroupDisplay';
+import UserDisplay from '../Users/UserDisplay';
+import GroupRequestLeaveButton from '../Buttons/GroupRequestLeaveButton';
+
+const Group = () => {
+    const {groupid} = useParams();
+
+    const authorized = bookStore((state) => state.authorized);
+    const setAuthorized = bookStore((state) => state.setAuthorized);
+    const setSiteError = bookStore((state) => state.setSiteError);
+
+    const loggedData = useFetchLogged([authorized, setAuthorized, setSiteError]);
+    const groupData = useFetchGroups([authorized, setAuthorized, setSiteError]);
+    const current_group = groupData.data.groups.find(group => group.group.id === groupid);
+
+    return (
+        <div>
+            <div>
+                <GroupDisplay props={[current_group, 'page']} />
+
+                {loggedData.data.logged_user.id === current_group.group.moderator ?
+                    <Link to={`/api/group/update/${current_group.id}`}>
+                        <PencilIcon className='h-6' />
+
+                        Edit group info
+                    </Link>
+                :
+                    <GroupRequestLeaveButton props={[
+                        loggedData.data.logged_user, 
+                        current_group, 
+                        current_group.members.some((member) => member.id === loggedData.data.logged_user.id)
+                    ]} />
+                }                 
+            </div>
+
+            <div>
+                {loggedData.data.logged_user.id === current_group.group.moderator ?
+                    <div>
+                        Requests
+
+                        {current_group.requests.length === 0 ?
+                            "No requests"
+                        :   
+                            <div>
+                                <ul>
+                                    {current_group.requests.forEach(request => {
+                                        return (
+                                            <li>
+                                                <UserDisplay props={[request, '']} />
+                                            </li>
+                                        )
+                                    })}
+                                </ul>
+
+                                {current_group.requests && 
+                                    <p> View {current_group.requests.length} requests </p>
+                                }
+
+                            </div>
+                        }
+                    </div>
+                :
+                    null
+                }
+                
+                <div>
+                    Members 
+
+                    <ul>
+                        {current_group.members.length === 0 ?
+                            "No members yet"
+                        :
+                            current_group.members.slice(0, 3).map(member => {
+                                return (
+                                    <li>
+                                        <UserDisplay props={[member, '']} />
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+
+                    <Link to={`/api/group/members/${current_group.id}`}>
+                        View all members
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Group;
