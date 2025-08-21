560b32fd9e8c727b50c58ec1309b0e55f8f5f509
Jat97 <85377239+Jat97@users.noreply.github.com>
2025-08-21 01:55:07 -0400

    Edited get_all_chats to include messages in chats array

--- express-bookmark/controllers/chatController.js
+++ express-bookmark/controllers/chatController.js
@@ -17,30 +17,43 @@ exports.get_all_chats = async (req, res) => {
                 messages.sent AS sent,
                 messages.checked AS checked
                 FROM chats 
-                LEFT JOIN users ON users.id = chats.user_2
-                LEFT JOIN messages ON messages.id = chats.last_message_sent
+                INNER JOIN users ON users.id = chats.user_2
+                INNER JOIN messages ON messages.id = chats.last_message_sent
                 WHERE user_1 = $1`,
                 [user_key.logged_user.id]
             );
 
             const chats = [];
-
-            if(user_chats.rows.forEach(chat => {
-                chats.push({
+            
+            for(const chat of user_chats.rows) {
+                const new_chat = {
                     id: chat.id,
                     user: {
                         first_name: chat.first_name,
                         last_name: chat.last_name,
                         profile_picture: chat.profile_picture
                     },
-                    last_message_sent: {
-                        sender: chat.sender_id,
-                        text: chat.text,
-                        sent: chat.sent,
-                        checked: chat.checked
-                    }
-                });
-            }));
+                };
+
+                const messages = await db.query(
+                    `SELECT messages.id AS id,
+                    messages.text AS text,
+                    messages.image AS image,
+                    messages.sent AS sent,
+                    messages.checked AS checked,
+                    users.id AS userid
+                    users.first_name AS first_name,
+                    users.last_name AS last_name
+                    FROM messages 
+                    INNER JOIN users ON user.id = messages.sending_user
+                    INNER JOIN users ON user.id = messages.receiving_user
+                    WHERE (messages.sending_user = $1 AND messages.receiving_user = $2)
+                    OR (messages.sending_user = $2 AND messages.receiving_user = $1)`,
+                    [user_key.logged_user.id, new_chat.user.id]
+                );
+
+                chats.push({chat: new_chat, messages: messages.rows});
+            }
 
             res.status(200).json({chats: chats});
         }
@@ -53,36 +66,36 @@ exports.get_all_chats = async (req, res) => {
     }
 };
 
-exports.get_chat_messages = async (req, res) => {
-    try {
-        const user_key = validateToken(req, res);
+// exports.get_chat_messages = async (req, res) => {
+//     try {
+//         const user_key = validateToken(req, res);
 
-        if(user_key) {
-            const messages = await db.query(
-                `SELECT messages.id AS id,
-                messages.sending_user AS sender,
-                messages.receiving_user AS receiver,
-                messages.text AS text,
-                messages.sent AS sent,
-                messages.checked AS checked
-                FROM messages
-                LEFT JOIN users AS sender ON sender.id = messages.sending_user
-                LEFT JOIN users AS receiver ON receiver.id = messages.receiving_user
-                WHERE (sender = $1 AND receiver = $2)
-                OR (sender = $2 AND receiver = $1)`,
-                [user_key.logged_user.id, req.params.userid]
-            );
+//         if(user_key) {
+//             const messages = await db.query(
+//                 `SELECT messages.id AS id,
+//                 messages.sending_user AS sender,
+//                 messages.receiving_user AS receiver,
+//                 messages.text AS text,
+//                 messages.sent AS sent,
+//                 messages.checked AS checked
+//                 FROM messages
+//                 LEFT JOIN users AS sender ON sender.id = messages.sending_user
+//                 LEFT JOIN users AS receiver ON receiver.id = messages.receiving_user
+//                 WHERE (sender = $1 AND receiver = $2)
+//                 OR (sender = $2 AND receiver = $1)`,
+//                 [user_key.logged_user.id, req.params.userid]
+//             );
 
-            res.status(200).json({messages: messages});
-        }
-        else {
-            res.status(401).send();
-        }
-    }
-    catch (err) {
-        res.status(500).json({error: err});
-    }
-};
+//             res.status(200).json({messages: messages});
+//         }
+//         else {
+//             res.status(401).send();
+//         }
+//     }
+//     catch (err) {
+//         res.status(500).json({error: err});
+//     }
+// };
 
 exports.send_message = async (req, res) => {
     try {
