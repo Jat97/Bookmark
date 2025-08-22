284c46956a197118689d14e6c97fd1a6b8bfdbee
Jat97 <85377239+Jat97@users.noreply.github.com>
2025-08-22 01:54:11 -0400

    Renamed file to be plural; edited mutations import to specify imported file

---
+++ vite-bookmark/src/Routes/Buttons/GroupAcceptRejectButtons.jsx
@@ -0,0 +1,35 @@
+import {useGroupAcceptMutation, useGroupRejectMutation} from '../Functions/Mutations/GroupMutations';
+import {bookStore} from '../../Context/bookStore';
+
+const GroupAcceptRejectMutation = (props) => {
+    const group = props.props[0];
+    const request = props.props[1];
+
+    const setSiteError = bookStore((state) => state.setSiteError);
+
+    const group_accept_mutation = useGroupAcceptMutation([group, request, setSiteError]);
+    const group_reject_mutation = useGroupRejectMutation([group, request, setSiteError]);
+
+    const handleGroupRequest = (e) => {
+        if(e.target.id === 'accept') {
+            return group_accept_mutation.mutate();
+        }
+        else {
+            return group_reject_mutation.mutate();
+        }
+    }
+
+    return (
+        <div>
+            <button type='button' id='accept' onClick={(e) => handleGroupRequest(e)}>
+                Accept request
+            </button>
+
+            <button type='button' id='reject' onClick={(e) => handleGroupRequest(e)}>
+                Reject request
+            </button>
+        </div>
+    )
+}
+
+export default GroupAcceptRejectMutation;
