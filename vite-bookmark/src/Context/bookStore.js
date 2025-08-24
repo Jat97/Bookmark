9d91735893ba2b51db3782ad5200a4ae4d29103b
Jat97 <85377239+Jat97@users.noreply.github.com>
2025-08-24 01:36:22 -0400

    Added textRef state for textbox

--- vite-bookmark/src/Context/bookStore.js
+++ vite-bookmark/src/Context/bookStore.js
@@ -3,8 +3,10 @@ import {create} from 'zustand';
 export const bookStore = create((set) => ({
     authorized: true,
     selected_chat: null,
+    textRef: null,
     siteError: null,
     setAuthorized: (bool) => set(() => ({authorized: bool})),
     setSelectedChat: (user) => set(() => ({selected_chat: user})),
+    setTextEditor: (item) => set(() => ({textRef: item})),
     setSiteError: (error) => set(() => ({siteError: error ? error : null}))
 }));
