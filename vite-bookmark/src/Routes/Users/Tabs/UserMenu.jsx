781883447cdb4e6090654c5a15b19fab2b23be31
Jat97 <85377239+Jat97@users.noreply.github.com>
2025-08-24 01:07:58 -0400

    Added component to display list of links to user information

---
+++ vite-bookmark/src/Routes/Users/Tabs/UserMenu.jsx
@@ -0,0 +1,45 @@
+import {Link} from 'react-router-dom';
+import {useLogOutMutation} from '../Functions/Mutations/UserMutations';
+import {bookStore} from '../../Context/bookStore';
+
+const UserMenu = (props) => {
+    const user = props.props;
+
+    const setSiteError = bookStore((state) => state.setSiteError);
+
+    const logout_mutation = useLogOutMutation(setSiteError);
+
+    const endSession = () => {
+        logout_mutation.mutate();
+    }
+
+    return (
+        <div>
+            <Link to={`/api/user/${user.id}`}> 
+                Profile 
+            </Link>
+
+            <Link to={`/api/user/friends/${user.id}`}>
+                View friends
+            </Link>
+
+            <Link to={`/api/user/groups/${user.id}`}> 
+                Groups
+            </Link>
+
+            <Link to={`/api/users`}>
+                View all users
+            </Link>
+
+            <Link to={`/api/groups`}>
+                View all groups
+            </Link>
+
+            <button type='button' onClick={() => endSession()}>
+                Logout
+            </button>
+        </div>
+    ) 
+}   
+
+export default UserMenu;
