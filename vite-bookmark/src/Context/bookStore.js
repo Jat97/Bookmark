9d44cf7e62ab470dd55625766508881a587253b6
Jat97 <85377239+Jat97@users.noreply.github.com>
2025-08-24 01:02:55 -0400

    Added selected_chat state to control the active chat in the user's chat list

--- vite-bookmark/src/Context/bookStore.js
+++ vite-bookmark/src/Context/bookStore.js
@@ -2,7 +2,9 @@ import {create} from 'zustand';
 
 export const bookStore = create((set) => ({
     authorized: true,
+    selected_chat: null,
     siteError: null,
     setAuthorized: (bool) => set(() => ({authorized: bool})),
+    setSelectedChat: (user) => set(() => ({selected_chat: user})),
     setSiteError: (error) => set(() => ({siteError: error ? error : null}))
 }));
