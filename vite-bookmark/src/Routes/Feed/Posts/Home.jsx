e98819c7a3aed8491a3d8f712c8c9a7bc60e1469
Jat97 <85377239+Jat97@users.noreply.github.com>
2025-09-07 02:29:55 -0400

    Added homepage component

---
+++ vite-bookmark/src/Routes/Feed/Posts/Home.jsx
@@ -0,0 +1,50 @@
+import {useFetchPosts}  from "../../Functions/Queries/PostQueries";
+import {useFetchFriends} from '../../Functions/Queries/UserQueries';
+import {bookStore} from '../../../Context/bookStore';
+import PostCard from './PostCard';
+import TextBox from '../../Miscellaneous/Inputs/TextBox';
+import NoItems from '../../Miscellaneous/Text/NoItems';
+import UserDisplay from '../../Users/UserDisplay';
+
+const Home = () => {
+    const authorized = bookStore((state) => state.authorized);
+    const setAuthorized = bookStore((state) => state.setAuthorized);
+    const setSiteError = bookStore((state) => state.setSiteError);
+
+    const postData = useFetchPosts([authorized, setAuthorized, setSiteError]);
+    const friendData = useFetchFriends([authorized, setAuthorized, setSiteError]);
+
+    const currently_online = friendData.data.friends.filter(friend => friend.online);
+
+    return (
+        <div>
+            <div>
+                <div>
+                    <TextBox />
+                </div>
+            
+                <div>
+                    {postData.data.posts.map(post => {
+                        return <PostCard props={post} />
+                    })}
+                </div>  
+            </div>
+
+            <div>
+                <div>
+                    Who's online?
+
+                    {currently_online.length === 0 ?
+                        <NoItems props={`No users online.`} />
+                    :
+                        currently_online.map(friend => {
+                            return <UserDisplay props={friend} />
+                        })
+                    }
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Home;
