b1b084589e0418a1e9d6aa8fa3005543932f9de2
Jat97 <85377239+Jat97@users.noreply.github.com>
2025-09-07 01:13:11 -0400

    Added component to display profile information

---
+++ vite-bookmark/src/Routes/Users/UserPage.jsx
@@ -0,0 +1,44 @@
+import {useParams} from 'react-router-dom';
+import {useFetchLogged, useFetchUsers} from '../Functions/Queries/UserQueries';
+import {useFetchPosts} from '../Functions/Queries/PostQueries';
+import {bookStore} from '../../Context/bookStore';
+import UserDisplay from './UserDisplay';
+import PostCard from '../Posts/PostCard';
+import FriendButton from '../Buttons/FriendButton';
+import BlockButton from '../Buttons/BlockButton';
+
+const UserPage = () => {
+    const {userid} = useParams();
+    const authorized = bookStore((state) => state.authorized);
+    const setAuthorized = bookStore((state) => state.setAuthorized);
+    const setSiteError = bookStore((state) => state.setSiteError);
+
+    const loggedData = useFetchLogged([authorized, setAuthorized, setSiteError]);
+    const userData = useFetchUsers([authorized, setAuthorized, setSiteError]);
+
+    const current_user = userData.data.users.find(user => user.id === userid);
+    const user_posts = postData.data.posts.filter(post => post.original_poster.id === current_user.id);
+
+    return (
+        <div>
+            <div>
+                <UserDisplay props={[current_user, loggedData.data.logged_user, '']} />
+
+                {current_user.id !== loggedData.data.logged_user.id &&
+                    <div>
+                        <FriendButton props={current_user} />
+                        <BlockButton props={current_user} />
+                    </div>
+                }
+            </div>
+
+            <div>
+                {user_posts.map(post => {
+                    return <PostCard props={post} />
+                })}
+            </div>
+        </div>
+    )
+};
+
+export default UserPage;
