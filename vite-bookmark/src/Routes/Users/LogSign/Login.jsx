84717eb02a0d98c7de99391d21fa8817423c82d2
Jat97 <85377239+Jat97@users.noreply.github.com>
2025-09-07 01:11:42 -0400

    Created components to handle logging in and signing up

---
+++ vite-bookmark/src/Routes/Users/LogSign/Login.jsx
@@ -0,0 +1,49 @@
+import {useState} from 'react';
+import {Link} from 'react-router-dom';
+import LogSignInput from '../../Inputs/LogSignInput';
+import InputErr from '../../Miscellaneous/Text/Errors/InputErr';
+
+const Login = () => {
+    const [logErrors, setLogErrors] = useState({
+        username: null,
+        password: null
+    });
+
+    return (
+        <div>
+            <div>
+                Sign in to Bookmark
+
+                <div>
+                    <div>
+                        Username
+
+                        <LogSignInput props={'username'} />
+
+                        {logErrors.username &&
+                            <InputErr props={logErrors.username} />
+                        }
+                    </div>
+
+                    <div>
+                        Password
+
+                        <LogSignInput props={'password'} />
+
+                        {logErrors.password &&
+                            <InputErr props={logErrors.password} />
+                        }
+                    </div>
+                </div>
+
+                <div>
+                    <button type='button'> Log in </button>
+
+                    Don't have an account? <Link to='/api/signup'> Create one! </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
---
+++ vite-bookmark/src/Routes/Users/LogSign/Signup.jsx
@@ -0,0 +1,76 @@
+import {useState} from 'react';
+import {Link} from 'react-router-dom';
+import LogSignInput from '../../Miscellaneous/Inputs/LogSignInput';
+import InputErr from '../../Miscellaneous/Text/Errors/InputErr';
+
+const Signup = () => {
+    const [signErrors, setSignErrors] = useState({
+        first_name: null,
+        last_name: null,
+        email: null,
+        password: null,
+        confirm: null
+    });
+
+    return (
+        <div>
+            <div>
+                Join us at Bookmark!
+
+                <div>
+                    <div>
+                        First Name
+
+                        <LogSignInput props='first_name' />
+
+                        {signErrors.first_name &&
+                            <InputErr props={signErrors.first_name} />
+                        }
+                    </div>
+
+                    <div>
+                        Last Name
+
+                        <LogSignInput props='last_name' />
+
+                        {signErrors.last_name && 
+                            <InputErr props={signErrors.last_name} />
+                        }
+                    </div>
+
+                    <div>
+                        Email
+
+                        <LogSignInput props='email' />
+
+                        {signErrors.email &&
+                            <InputErr props={signErrors.email} />
+                        }
+                    </div>
+
+                    <div>
+                        Password
+
+                        <LogSignInput props='password' />
+
+                        {signErrors.password &&
+                            <InputErr props={signErrors.password} />
+                        }
+                    </div>
+
+                    <div>
+                        Confirm
+
+                        <LogSignInput props='confirm' />
+
+                        {signErrors.confirm &&
+                            <InputErr props={signErrors.confirm} />
+                        }
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Signup;
