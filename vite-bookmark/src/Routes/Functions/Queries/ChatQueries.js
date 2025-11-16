7156d5bf31f9be12129f66387af3d4292f1d1438
Jat97 <85377239+Jat97@users.noreply.github.com>
2025-11-16 00:27:21 -0500

    Removed messages query

--- vite-bookmark/src/Routes/Functions/Queries/ChatQueries.js
+++ vite-bookmark/src/Routes/Functions/Queries/ChatQueries.js
@@ -4,7 +4,7 @@ export const useFetchChats = ([authorized, setAuthorized, setSiteError]) => {
     const result = useQuery({
         queryKey: ['chats'],
         queryFn: async () => {
-            return await fetch('http://127.0.0.1:9000/api/chats', {
+            return await fetch('http://localhost:9000/api/chats', {
                 method: 'GET',
                 credentials: 'include'
             })
@@ -30,32 +30,34 @@ export const useFetchChats = ([authorized, setAuthorized, setSiteError]) => {
     return result;
 };
 
-export const useFetchMessages = ([userid, authorized, setAuthorized, setSiteError]) => {
-    const result = useQuery({
-        queryKey: ['messages'], 
-        queryFn: async () => {
-            return await fetch(`http://127.0.0.1:9000/api/chat/${userid}`, {
-                method: 'GET',
-                credentials: 'include'
-            })
-            .then(res => {
-                if(res.status === 401) {
-                    setAuthorized(false);
-                }
-                else if(!res.ok) {
-                    throw Error(`Error ${res.status}: ${res.statusText}`);
-                }
-                else {
-                    if(!authorized) {
-                        setAuthorized(true);
-                    }
+// export const useFetchMessages = ([userid, authorized, setAuthorized, setSiteError]) => {
+//     const result = useQuery({
+//         queryKey: ['messages'], 
+//         queryFn: async () => {
+//             return await fetch(`http://127.0.0.1:9000/api/chat/${userid}`, {
+//                 method: 'GET',
+//                 credentials: 'include'
+//             })
+//             .then(res => {
+//                 console.log(res);
+                
+//                 if(res.status === 401) {
+//                     setAuthorized(false);
+//                 }
+//                 else if(!res.ok) {
+//                     throw Error(`Error ${res.status}: ${res.statusText}`);
+//                 }
+//                 else {
+//                     if(!authorized) {
+//                         setAuthorized(true);
+//                     }
 
-                    return res.json();
-                }
-            })
-            .catch(err => setSiteError(err))
-        }
-    });
+//                     return res.json();
+//                 }
+//             })
+//             .catch(err => setSiteError(err))
+//         }
+//     });
 
-    return result;
-};
+//     return result;
+// };
