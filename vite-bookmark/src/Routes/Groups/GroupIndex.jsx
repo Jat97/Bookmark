d982fbd4c7befcd0fc089739fbe42b357b8b519d
Jat97 <85377239+Jat97@users.noreply.github.com>
2025-11-18 01:19:52 -0500

    Added component to display all groups

---
+++ vite-bookmark/src/Routes/Groups/GroupIndex.jsx
@@ -0,0 +1,35 @@
+import {useFetchLogged} from '../Functions/Queries/UserQueries';
+import {useFetchGroups} from '../Functions/Queries/GroupQueries';
+import {useBookStore} from '../../Context/bookStore';
+import GroupDisplay from './GroupDisplay';
+import GroupRequestLeaveButton from '../Buttons/GroupRequestLeaveButton'
+import PageHeader from '../Miscellaneous/Text/PageHeader';
+
+const GroupIndex = () => {
+    const authorized = useBookStore((state) => state.authorized);
+    const setAuthorized = useBookStore((state) => state.setAuthorized);
+    const setSiteError = useBookStore((state) => state.setSiteError);
+
+    const loggedData = useFetchLogged([authorized, setAuthorized, setSiteError]);
+    const groupData = useFetchGroups([authorized, setAuthorized, setSiteError]);
+
+    return (
+        <div>
+            <PageHeader props={'All groups'} />
+
+            {groupData.data?.groups?.map(group => {
+                const is_member = group.members?.some((user) => user.id === loggedData.data?.logged_user.id);
+
+                return (
+                    <div>
+                        <GroupDisplay props={[group.group, '']} />
+
+                        <GroupRequestLeaveButton props={[loggedData.data?.logged_user, group.group, is_member]} />
+                    </div>
+                )
+            })}
+        </div>    
+    )
+}
+
+export default GroupIndex;
