350e7421e85da068a1ac7fe594d9c40ae53d10c6
Jat97 <85377239+Jat97@users.noreply.github.com>
2025-11-24 00:45:06 -0500

    Added styling

--- vite-bookmark/src/Routes/Miscellaneous/Inputs/LogSignInput.jsx
+++ vite-bookmark/src/Routes/Miscellaneous/Inputs/LogSignInput.jsx
@@ -3,8 +3,10 @@ const LogSignInput = (props) => {
 
     return (
         <input data-testid={id} name={id} type={id === 'password' || id === 'confirm' ? 'password' : id === 'email' ? 'email' : 'text'}
+            className='bg-gray-200 p-1 max-w-12 focus:bg-white' 
             minLength={id === 'password' || id === 'confirm' ? '8' : '1'} 
-            maxLength={id === 'password' || id === 'confirm' ? '8' : '1'}>
+            maxLength={id === 'password' || id === 'confirm' ? '8' : '1'}
+            placeholder='Enter information here...'>
         </input>
     )
 }
