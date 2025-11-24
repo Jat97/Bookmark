e2977fdcfdd64b437d98254d2013d20d6407043c
Jat97 <85377239+Jat97@users.noreply.github.com>
2025-11-24 00:44:24 -0500

    Added styling and rearranged components to create boxes around inputs

--- vite-bookmark/src/Routes/Users/LogSign/Login.jsx
+++ vite-bookmark/src/Routes/Users/LogSign/Login.jsx
@@ -1,35 +1,72 @@
 import {useState} from 'react';
 import {Link} from 'react-router-dom';
+import {useBookStore} from '../../../Context/bookStore';
 import LogSignInput from '../../Miscellaneous/Inputs/LogSignInput';
 import InputErr from '../../Miscellaneous/Text/Errors/InputErr';
+import LogSignButton from '../../Buttons/LogSignButton';
 
 const Login = () => {
+    const setSiteError = useBookStore((state) => state.setSiteError);
+
     const [logErrors, setLogErrors] = useState({
-        username: null,
+        user: null,
         password: null
     });
 
+    const logIn = () => {
+        fetch('http://localhost:9000/api/login', {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                user: user,
+                password: password
+            })
+        })
+        .then(res => {
+            if(!res.ok) {
+                throw Error(`Error ${res.status}: ${res.statusText}`);
+            }
+            else {
+                return res.json();
+            }
+        })
+        .then(json => {
+            setLogErrors({
+                user: json.user_err ? json.user_err : null,
+                password: json.pass_err ? json.pass_err : null
+            });
+        })
+        .catch(err => setSiteError(err.message));
+    }
+
     return (
-        <div>
-            <div>
+        <div className='absolute top-1/2 left-1/2'>
+            <div className='border border-slate-200 shadow-sm shadow-slate-200 bg-white'>
                 Sign in to Bookmark
 
                 <div>
                     <div>
-                        Username
+                        <div className='font-semibold flex flex-col items-start'>
+                            Username
 
-                        <LogSignInput props={'email'} />
-
+                            <LogSignInput props={'email'} />
+                        </div>
+                            
                         {logErrors.username &&
                             <InputErr props={logErrors.username} />
-                        }
+                        } 
                     </div>
 
                     <div>
-                        Password
+                        <div className='font-semibold flex flex-col items-start'>
+                            Password
 
-                        <LogSignInput props={'password'} />
-
+                            <LogSignInput props={'password'} />
+                        </div>
+                        
                         {logErrors.password &&
                             <InputErr props={logErrors.password} />
                         }
@@ -37,9 +74,9 @@ const Login = () => {
                 </div>
 
                 <div>
-                    <button type='button'> Log in </button>
+                    <LogSignButton props={['Log in', logIn]} />
 
-                    Don't have an account? <Link to='/api/signup'> Create one! </Link>
+                    Don't have an account? <Link to='/api/signup' className='text-blue-600 underline'> Create one! </Link>
                 </div>
             </div>
         </div>
--- vite-bookmark/src/Routes/Users/LogSign/Signup.jsx
+++ vite-bookmark/src/Routes/Users/LogSign/Signup.jsx
@@ -1,9 +1,12 @@
 import {useState} from 'react';
 import {Link} from 'react-router-dom';
+import {useBookStore} from '../../../Context/bookStore';
 import LogSignInput from '../../Miscellaneous/Inputs/LogSignInput';
 import InputErr from '../../Miscellaneous/Text/Errors/InputErr';
 
 const Signup = () => {
+    const setSiteError = useBookStore((state) => state.setSiteError);
+
     const [signErrors, setSignErrors] = useState({
         first_name: null,
         last_name: null,
@@ -12,16 +15,53 @@ const Signup = () => {
         confirm: null
     });
 
+    const signUp = () => {
+        fetch('http://localhost:9000/api/signup', {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                first_name: first_name,
+                last_name: last_name,
+                email: email,
+                password: password,
+                confirm: confirm
+            })
+        })
+        .then(res => {
+            if(!res.ok) {
+                throw Error(`Error ${res.status}: ${res.statusText}`);
+            }
+            else {
+                return res.json();
+            }
+        })
+        .then(json => {
+            json.errors.errors.forEach(error => {
+                setSignErrors(prevState => ({
+                    ...prevState,
+                    [error.param] : err.msg
+                }));
+            })
+        })
+        .catch(err => setSiteError(err.message))
+    }
+
     return (
-        <div>
-            <div>
+        <div className='absolute top-1/3 right-1/3'>
+            <div className='flex flex-col items-center border border-slate-200 shadow-sm shadow-slate-200'>
                 Join us at Bookmark!
 
-                <div>
+                <div className='font-semibold grid grid-cols-2 items-center'>
                     <div>
-                        First Name
+                        <div className='font-semibold flex flex-col items-start'>
+                            First Name
 
-                        <LogSignInput props='first_name' />
+                            <LogSignInput props='first_name' />
+                        </div>
+                        
 
                         {signErrors.first_name &&
                             <InputErr props={signErrors.first_name} />
@@ -29,9 +69,11 @@ const Signup = () => {
                     </div>
 
                     <div>
-                        Last Name
+                        <div className='font-semibold flex flex-col items-start'>
+                            Last Name
 
-                        <LogSignInput props='last_name' />
+                            <LogSignInput props='last_name' />
+                        </div>
 
                         {signErrors.last_name && 
                             <InputErr props={signErrors.last_name} />
@@ -39,9 +81,12 @@ const Signup = () => {
                     </div>
 
                     <div>
-                        Email
+                        <div className='font-semibold flex flex-col items-start'>
+                            Email
 
-                        <LogSignInput props='email' />
+                            <LogSignInput props='email' />
+                        </div>
+                       
 
                         {signErrors.email &&
                             <InputErr props={signErrors.email} />
@@ -49,9 +94,12 @@ const Signup = () => {
                     </div>
 
                     <div>
-                        Password
+                        <div className='font-semibold flex flex-col items-start'>
+                            Password
 
-                        <LogSignInput props='password' />
+                            <LogSignInput props='password' />
+                        </div>
+                        
 
                         {signErrors.password &&
                             <InputErr props={signErrors.password} />
@@ -59,22 +107,25 @@ const Signup = () => {
                     </div>
 
                     <div>
-                        Confirm
+                        <div className='flex flex-col items-start'>
+                            Confirm
 
-                        <LogSignInput props='confirm' />
+                            <LogSignInput props='confirm' />
+                        </div>
+                        
 
                         {signErrors.confirm &&
                             <InputErr props={signErrors.confirm} />
                         }
                     </div>
+                </div>
+                
+                <div className='flex flex-col items-center'>
+                    <LogSignButton props={['Create account', signUp]} />
 
-                    <div>
-                        <button type='button'>
-                            Create account
-                        </button>
-
-                        Already have an account? <Link to='/api/login'> Log in here! </Link>
-                    </div>
+                    Already have an account? <Link to='/api/login' className='text-blue-600 underline'> 
+                        Log in here! 
+                    </Link>
                 </div>
             </div>
         </div>
