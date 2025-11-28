921c11c9023b3891b652791c3535b73c15aafc4e
Jat97 <85377239+Jat97@users.noreply.github.com>
2025-11-28 00:09:38 -0500

    Added helper functions for styling, and changed p elements to Links

--- vite-bookmark/src/Routes/Users/UserDisplay.jsx
+++ vite-bookmark/src/Routes/Users/UserDisplay.jsx
@@ -1,3 +1,4 @@
+import {Link} from 'react-router-dom';
 import {useEditPictureMutation} from '../Functions/Mutations/UserMutations';
 import {useBookStore} from '../../Context/bookStore';
 import {CameraIcon, UserIcon} from '@heroicons/react/24/solid';
@@ -15,25 +16,46 @@ const UserDisplay = (props) => {
         return picture_mutation.mutate();
     }
 
+    const handleUserImageCSS = () => {
+        if(user_mode === 'index') {
+            return 'max-w-16 md:max-w-20';
+        }
+        else if(user_mode === 'profile') {
+            return 'max-w-20 md:max-w-24';
+        }
+    }
+
+    const handleUserIconCSS = () => {
+        if(user_mode === 'index') {
+            return 'max-h-5 md:max-h-6';
+        }
+        else if(user_mode === 'profile') {
+            return 'max-h-8 md:max-h-10'
+        }
+    }
+
     return (
         <div>
             <div>
                 <div>
                     {user.profile_picture ? 
-                        <img src={user.profile_picture}></img>
+                        <img src={user.profile_picture} className={`${handleUserImageCSS()}`}></img>
                     :
-                        <UserIcon className='h-6' />
+                        <UserIcon className={`${handleUserIconCSS()}`} />
                     }
 
                     {user.id === logged.id &&
                         <label onChange={() => editProfilePicture()}>
                             <input type='file' className='hidden'></input>
-                            <CameraIcon className='h-6' />
+                            <CameraIcon className='h-6 fill-orange-200' />
                         </label>
                     }
                 </div>
                 
-                <p> {`${user.first_name} ${user.last_name}`} </p>
+                <Link to={`/api/profile/${user.id}`} className={`font-semibold 
+                    ${user_mode === 'index' ? 'text-blue-600 hover:underline' : 'cursor-not-allowed'}`}> 
+                    {`${user.first_name} ${user.last_name}`} 
+                </Link>
             </div>
         </div>
     )
