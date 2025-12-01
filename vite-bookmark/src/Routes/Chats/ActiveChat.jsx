44c1d0dc085c3c6ab8364a6293ab406183ecf84e
Jat97 <85377239+Jat97@users.noreply.github.com>
2025-12-01 01:09:31 -0500

    Added conditional to render either empty chat queue message or selected chat

--- vite-bookmark/src/Routes/Chats/ActiveChat.jsx
+++ vite-bookmark/src/Routes/Chats/ActiveChat.jsx
@@ -1,5 +1,5 @@
 import {useState} from 'react';
-import {PaperClipIcon, PaperAirplaneIcon, XMarkIcon} from '@heroicons/react/24/solid';
+import {ChatBubbleLeftEllipsisIcon, PaperClipIcon, PaperAirplaneIcon, XMarkIcon} from '@heroicons/react/24/solid';
 import {useBookStore} from '../../Context/bookStore';
 import {useSendMessageMutation} from '../Functions/Mutations/ChatMutations';
 import Message from './Message';
@@ -35,42 +35,52 @@ const ActiveChat = (props) => {
 
     return (
         <div>
-            <div className='flex flex-col items-center'>
-                {`${chat.user_2.first_name} ${chat.user_2.last_name}`}
-            </div>
-
-            {chat.messages.map(message => {
-                return <Message props={message} />
-            })}
-
-            <div>
-                {image.reader &&
-                    <div className='border border-slate-200'>
-                        <div className='float-right bg-zinc-400 rounded-full p-1 hover:bg-slate-100'
-                             onClick={() => removeImage()}>
-                            <XMarkIcon className='h-6' />
-                        </div>
-                        
-                       <img src={image.reader}></img> 
-                    </div>   
-                }
-
+            {selected_chat !== undefined ? 
                 <div>
-                    <input type='text' className='bg-slate-200 max-w-20 rounded-full md:max-w-28 focus:bg-white'
-                        placeholder='What do you want to say?'>
-                    </input>
+                    <ChatBubbleLeftEllipsisIcon className='max-h-12 fill-slate-200' />
 
-                    <label>
-                        <input type='file' id='file_upload' className='hidden' onChange={() => uploadImage()}></input>
-                        <PaperClipIcon className='h-6 fill-orange-200' />
-                    </label>
-
-                    <button type='button' className='border border-orange-200 rounded-full hover:bg-amber-100' 
-                        onClick={() => sendMessage()}>
-                        <PaperAirplaneIcon className='h-6 fill-orange-200' />
-                    </button>
+                    <p className='text-slate-200 font-semibold'> You don't seem to have any chats! </p>
                 </div>
-            </div>
+            :
+                <div>
+                    <div className='flex flex-col items-center'>
+                        {`${chat.user_2.first_name} ${chat.user_2.last_name}`}
+                    </div>
+
+                    {chat.messages.map(message => {
+                        return <Message props={message} />
+                    })}
+
+                    <div>
+                        {image.reader &&
+                            <div className='border border-slate-200'>
+                                <div className='float-right bg-zinc-400 rounded-full p-1 hover:bg-slate-100'
+                                    onClick={() => removeImage()}>
+                                    <XMarkIcon className='h-6' />
+                                </div>
+                                
+                                <img src={image.reader}></img> 
+                            </div>   
+                        }
+
+                        <div>
+                            <input type='text' className='bg-slate-200 max-w-20 rounded-full md:max-w-28 focus:bg-white'
+                                placeholder='What do you want to say?'>
+                            </input>
+
+                            <label>
+                                <input type='file' id='file_upload' className='hidden' onChange={() => uploadImage()}></input>
+                                <PaperClipIcon className='h-6 fill-orange-200' />
+                            </label>
+
+                            <button type='button' className='border border-orange-200 rounded-full hover:bg-amber-100' 
+                                onClick={() => sendMessage()}>
+                                <PaperAirplaneIcon className='h-6 fill-orange-200' />
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            }
         </div>
     )
 }
