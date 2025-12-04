4c75fe5ed0e38143d4c7bd57194180a289fd10c9
Jat97 <85377239+Jat97@users.noreply.github.com>
2025-12-04 01:28:30 -0500

    Updated width and border shape

--- vite-bookmark/src/Routes/Buttons/LogSignButton.jsx
+++ vite-bookmark/src/Routes/Buttons/LogSignButton.jsx
@@ -3,7 +3,8 @@ const LogSignButton = (props) => {
     const log_sign_fn = props.props[1];
 
     return (
-        <button type='button' className='text-white bg-sky-400 max-w-16 hover:bg-cyan-100' onClick={log_sign_fn}>
+        <button type='button' className='text-white bg-sky-400 rounded-full w-[100px] hover:bg-cyan-100' 
+            onClick={log_sign_fn}>
             {log_sign_text}
         </button>
     )
