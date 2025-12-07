73ba77f887fbd39cf1b2ce6069221e9e76073b45
Jat97 <85377239+Jat97@users.noreply.github.com>
2025-12-07 00:05:02 -0500

    Adjusted width to 150 pixels regardless of screen size

--- vite-bookmark/src/Routes/Buttons/LogSignButton.jsx
+++ vite-bookmark/src/Routes/Buttons/LogSignButton.jsx
@@ -3,8 +3,8 @@ const LogSignButton = (props) => {
     const log_sign_fn = props.props[1];
 
     return (
-        <button type='button' className='text-white cursor-pointer bg-sky-400 rounded-full p-1 w-[150px] 
-            md:w-[100px] hover:bg-cyan-100' 
+        <button type='button' className='text-white font-semibold cursor-pointer bg-sky-400 rounded-full p-1 w-[150px] 
+            hover:bg-cyan-100' 
             onClick={log_sign_fn}>
             {log_sign_text}
         </button>
