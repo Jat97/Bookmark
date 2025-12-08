105ede8c9cefc3c7e30cb58fae7880055b925464
Jat97 <85377239+Jat97@users.noreply.github.com>
2025-12-08 02:18:07 -0500

    Added mutation functions with helper function to determine if the request is for a user or a group

--- vite-bookmark/src/Routes/Buttons/AcceptButton.jsx
+++ vite-bookmark/src/Routes/Buttons/AcceptButton.jsx
@@ -1,10 +1,29 @@
+import {useAcceptRequestMutation} from "../Functions/Mutations/FriendMutations";
+import {useGroupAcceptMutation} from '../Functions/Mutations/GroupMutations';
+import {useBookStore} from '../../Context/bookStore';
+
 const AcceptButton = (props) => {
-    const id = props.props[0];
-    const accept_fn = props.props[1];
+    const user_group = props.props;
+
+    const setSiteError = useBookStore((state) => state.setSiteError);
+
+    const friend_accept_mutation = useAcceptRequestMutation([user_group.id, setSiteError]);
+
+    const group_accept_mutation = useGroupAcceptMutation([user_group.id, setSiteError]);
+
+    const acceptRequest = () => {
+        if(user_group.first_name) {
+            return friend_accept_mutation.mutate();
+        }
+        else {
+            return group_accept_mutation.mutate();
+        }
+    }
 
     return (
-        <button id='accept' data-testid={`accept-${id}`} className='cursor-pointer bg-green-200 hover:bg-lime-100' 
-            onClick={accept_fn}>
+        <button id='accept' data-testid={`accept-${user_group.id}`} className='cursor-pointer bg-green-200 
+            hover:bg-lime-100' 
+            onClick={() => acceptRequest()}>
             Accept
         </button>
     )
