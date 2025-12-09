62af28de432c0d2b2734d7f09f43632841442ac2
Jat97 <85377239+Jat97@users.noreply.github.com>
2025-12-09 00:31:09 -0500

    Added Navbar

--- vite-bookmark/src/Routes/Feed/Posts/Home.jsx
+++ vite-bookmark/src/Routes/Feed/Posts/Home.jsx
@@ -5,6 +5,7 @@ import PostCard from './PostCard';
 import TextBox from '../../Miscellaneous/Inputs/TextBox';
 import NoItems from '../../Miscellaneous/Text/NoItems';
 import UserDisplay from '../../Users/UserDisplay';
+import Navbar from '../../Users/Navbar';
 
 const Home = () => {
     const authorized = useBookStore((state) => state.authorized);
@@ -14,33 +15,37 @@ const Home = () => {
     const postData = useFetchPosts([authorized, setAuthorized, setSiteError]);
     const friendData = useFetchFriends([authorized, setAuthorized, setSiteError]);
 
-    const currently_online = friendData.data.friends.filter(friend => friend.online);
+    const currently_online = friendData.data?.friends?.filter(friend => friend.online);
 
     return (
-        <div className='grid grid-cols-2 items-center'>
-            <div className='flex flex-col items-center'>
-                <div>
-                    <TextBox props={['', '']}/>
-                </div>
+        <div>
+            <Navbar />
             
+            <div className='grid grid-cols-2 items-center'>
+                <div className='flex flex-col items-center'>
+                    <div>
+                        <TextBox props={['', '']}/>
+                    </div>
+                
+                    <div>
+                        {postData.data?.posts.map(post => {
+                            return <PostCard props={post} />
+                        })}
+                    </div>  
+                </div>
+
                 <div>
-                    {postData.data?.posts.map(post => {
-                        return <PostCard props={post} />
-                    })}
-                </div>  
-            </div>
+                    <div className='border border-slate-200'>
+                        <p className='font-semibold'> Who's online? </p>
 
-            <div>
-                <div className='border border-slate-200'>
-                    <p className='font-semibold'> Who's online? </p>
-
-                    {currently_online.length === 0 ?
-                        <NoItems props={'No users online.'} />
-                    :
-                        currently_online.map(friend => {
-                            return <UserDisplay props={friend} />
-                        })
-                    }
+                        {currently_online?.length === 0 ?
+                            <NoItems props={'No users online.'} />
+                        :
+                            currently_online?.map(friend => {
+                                return <UserDisplay props={friend} />
+                            })
+                        }
+                    </div>
                 </div>
             </div>
         </div>
