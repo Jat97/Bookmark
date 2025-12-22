6fede3ba5b4a2cffbb6165c26cb4a151f3f5be81
Jat97 <85377239+Jat97@users.noreply.github.com>
2025-12-22 02:00:46 -0500

    Added box shadow to parent div; set and formatted post text; adjusted props for user and group displays to include logged variable

--- vite-bookmark/src/Routes/Feed/Posts/PostCard.jsx
+++ vite-bookmark/src/Routes/Feed/Posts/PostCard.jsx
@@ -6,15 +6,17 @@ const PostCard = (props) => {
     const post = props.props;
 
     return (
-        <div className='flex flex-col items-start'>
-            {post.original_poster ? 
-                <UserDisplay props={[post.original_poster, '']} /> 
-            :
-                <GroupDisplay props={[post.original_group, '']} />
-            }
-           
-            <p className='max-w-3/4'> {post.text} </p>
-
+        <div className='flex flex-col items-start border border-slate-200 shadow-sm-slate-200 md:w-full'>
+            <div className='flex flex-col items-start bg-white md:w-full'>
+                {post.original_poster ? 
+                    <UserDisplay props={[post.original_poster, false, 'post']} /> 
+                :
+                    <GroupDisplay props={[post.original_group, false, 'post']} />
+                }
+            
+                <p className='m-2 max-w-3/4' dangerouslySetInnerHTML={{__html: post.text}}></p>
+            </div>
+            
             <PostCommentBox props={post} />
         </div>
     )
