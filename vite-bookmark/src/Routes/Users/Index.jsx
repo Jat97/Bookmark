7fcf583209d9f5158e3595ff545929a5ee61affd
Jat97 <85377239+Jat97@users.noreply.github.com>
2025-12-26 01:26:20 -0500

    Added width to li element; added Navbar

--- vite-bookmark/src/Routes/Users/Index.jsx
+++ vite-bookmark/src/Routes/Users/Index.jsx
@@ -6,6 +6,7 @@ import UserDisplay from './UserDisplay';
 import FriendButton from '../Buttons/FriendButton';
 import BlockButton from '../Buttons/BlockButton';
 import PageHeader from '../Miscellaneous/Text/PageHeader';
+import Navbar from './Navbar';
 
 const Index = () => {
     const {groupid} = useParams();
@@ -17,17 +18,19 @@ const Index = () => {
     const groupData = groupid ? useFetchGroups([authorized, setAuthorized, setSiteError]) : null;
     const userData = groupid ? null : useFetchUsers([authorized, setAuthorized, setSiteError]);
 
-    const user_arr = groupData ? groupData.data.groups.members : userData.data.users;
+    const user_arr = groupData ? groupData.data.groups.members : userData.data?.users;
 
     return (
         <div>
+            <Navbar />
+            
             <PageHeader props={groupData ? 'Members' : 'Users'} />
 
             <ul className='flex flex-col items-center'>
-                {user_arr.map(user => {
+                {user_arr?.map(user => {
                     return (
-                        <li className='flex justify-around items-center'>
-                            <UserDisplay props={[user, '']} />
+                        <li className='flex justify-around items-center w-2/3'>
+                            <UserDisplay props={[user, false, 'index']} />
 
                             <div className='flex flex-col items-center'>
                                 <FriendButton props={user} />
