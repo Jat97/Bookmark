10def9ebef3ffc9c3384359050a8bff080be3fa2
Jat97 <85377239+Jat97@users.noreply.github.com>
2026-01-05 00:51:49 -0500

    Added create_group_tab

--- vite-bookmark/src/App.jsx
+++ vite-bookmark/src/App.jsx
@@ -3,13 +3,14 @@ import {useBookStore} from './Context/bookStore';
 import Login from './Routes/Users/LogSign/Login';
 import Signup from './Routes/Users/LogSign/Signup';
 import Home from './Routes/Feed/Posts/Home';
-import Index from './Routes/Users/Index';
-import GroupIndex from './Routes/Groups/GroupIndex';
+import Index from './Routes/Pages/Index';
 import ProfilePage from './Routes/Pages/ProfilePage';
 import FullPost from './Routes/Feed/Posts/FullPost';
+import CreateGroup from './Routes/Groups/CreateGroup';
 import SiteErr from './Routes/Miscellaneous/Text/Errors/SiteErr';
 
 function App() {
+    const create_group_tab = useBookStore((state) => state.create_group_tab);
     const error = useBookStore((state) => state.error);
     
     return (
@@ -20,13 +21,17 @@ function App() {
                     <Route path='/api/signup' element={<Signup />}></Route>
                     <Route path='/api/home' element={<Home />}></Route>
                     <Route path='/api/index/users' element={<Index />}></Route>
-                    <Route path='/api/index/groups' element={<GroupIndex />}></Route>
+                    <Route path='/api/index/groups' element={<Index />}></Route>
                     <Route path='/api/profile/:profileid' element={<ProfilePage />}></Route>
                     <Route path='/api/group/:profileid' element={<ProfilePage />}></Route>
                     <Route path='/api/post/:postid/comments' element={<FullPost />}></Route>
                     <Route path='*' element={<Navigate to='/api/login' />}></Route>
                 </Routes>
             </BrowserRouter>
+
+            {create_group_tab &&
+                <CreateGroup />
+            }
 
             {error &&
                 <SiteErr props={error} />
