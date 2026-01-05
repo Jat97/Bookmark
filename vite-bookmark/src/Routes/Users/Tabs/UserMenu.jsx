8be26fb208ff6f73499340e2501fec955e162b9d
Jat97 <85377239+Jat97@users.noreply.github.com>
2026-01-05 00:47:03 -0500

    Added tab to create group from menu

--- vite-bookmark/src/Routes/Users/Tabs/UserMenu.jsx
+++ vite-bookmark/src/Routes/Users/Tabs/UserMenu.jsx
@@ -5,9 +5,14 @@ import {useBookStore} from '../../../Context/bookStore';
 const UserMenu = (props) => {
     const user = props.props;
 
+    const setCreateGroupTab = useBookStore((state) => state.setCreateGroupTab);
     const setSiteError = useBookStore((state) => state.setSiteError);
 
     const logout_mutation = useLogOutMutation(setSiteError);
+
+    const enableGroupTab = () => {
+        setCreateGroupTab(true);
+    }
 
     const endSession = () => {
         logout_mutation.mutate();
@@ -15,7 +20,7 @@ const UserMenu = (props) => {
 
     return (
         <div className='absolute flex flex-col items-center gap-1 bg-orange-300 
-            max-h-[150px] w-[150px] md:left-[20px] md:max-h-52 md:w-[200px]'>
+            max-h-[150px] w-[150px] z-30 md:left-[20px] md:max-h-52 md:w-[200px]'>
             <Link to={`/api/profile/${user?.id}`} className='cursor-pointer text-center w-full hover:bg-amber-100'> 
                 Profile 
             </Link>
@@ -35,6 +40,10 @@ const UserMenu = (props) => {
             <Link to={`/api/index/groups`} className='cursor-pointer text-center w-full hover:bg-amber-100'>
                 View all groups
             </Link>
+            
+            <p className='cursor-pointer text-center w-full hover:bg-amber-100' onClick={() => enableGroupTab()}> 
+                Create group 
+            </p>
 
             <button type='button' className='cursor-pointer w-full hover:bg-amber-100' onClick={() => endSession()}>
                 Logout
