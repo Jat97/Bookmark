6aee0e304f590476d3f18afd7e1842d3aef8b6c3
Jat97 <85377239+Jat97@users.noreply.github.com>
2026-01-10 00:23:29 -0500

    Removed Navbar; added buttons to accept or reject friend requests

--- vite-bookmark/src/Routes/Pages/Index.jsx
+++ vite-bookmark/src/Routes/Pages/Index.jsx
@@ -1,51 +1,45 @@
-import {useLocation} from 'react-router-dom';
-import {useFetchLogged, useFetchUsers} from '../Functions/Queries/UserQueries';
-import {useFetchGroups} from '../Functions/Queries/GroupQueries';
-import {useBookStore} from '../../Context/bookStore';
 import FriendButton from '../Buttons/FriendButton';
 import BlockButton from '../Buttons/BlockButton';
 import GroupRequestLeaveButton from '../Buttons/GroupRequestLeaveButton';
+import AcceptButton from '../Buttons/AcceptButton';
+import RejectButton from '../Buttons/RejectButton';
 import PageHeader from '../Miscellaneous/Text/PageHeader';
 import ProfileDisplay from '../Miscellaneous/Images/ProfileDisplay';
-import Navbar from '../Users/Navbar';
 
-const Index = () => {
-    const location = useLocation();
-
-    const authorized = useBookStore((state) => state.authorized);
-    const setAuthorized = useBookStore((state) => state.setAuthorized);
-    const setSiteError = useBookStore((state) => state.setSiteError);
-
-    const groupData = useFetchGroups([authorized, setAuthorized, setSiteError]);
-    const userData = useFetchUsers([authorized, setAuthorized, setSiteError]);
-    const loggedData = useFetchLogged([authorized, setAuthorized, setSiteError]);
-
-    const index_arr = location.pathname.includes('groups') ? groupData.data.groups : userData.data.users;
+const Index = (props) => {
+    const logged = props.props[0];
+    const moderator = props.props[1];
+    const items = props.props[2];
 
     return (
-        <div>
-            <Navbar />
-            
+        <div>            
             <PageHeader props={location.pathname.includes('groups') ? 'Groups' : 'Users'} />
 
             <ul className='flex flex-col items-center'>
-                {index_arr?.map((item) => {
+                {items?.map((item) => {
                     return (
                         <li className='flex justify-around items-center w-2/3'>
                             <ProfileDisplay props={[item, false, 'index']} />
 
-                            {item.title ?
+                            {item.title && logged ?
                                 <GroupRequestLeaveButton props={[
                                     loggedData.data.logged_user, 
                                     item,
                                     item.members.some((member) => member.id === loggedData.data.logged_user.id)
                                 ]} />
                             :
-                                <div className='flex flex-col items-center'>
-                                    <FriendButton props={item} />
+                                item.title && moderator ?
+                                    <div>
+                                        <AcceptButton props={item} />
 
-                                    <BlockButton props={item} />
-                                </div>
+                                        <RejectButton props={item} />
+                                    </div>
+                                :
+                                    <div className='flex flex-col items-center'>
+                                        <FriendButton props={item} />
+
+                                        <BlockButton props={item} />
+                                    </div>
                             }
                         </li>
                     )
