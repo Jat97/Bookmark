11686d0d36a6b632c611e23c323b5607e00effa6
Jat97 <85377239+Jat97@users.noreply.github.com>
2026-01-10 11:31:07 -0500

    Added description_input variable with function to set its value

--- vite-bookmark/src/Context/bookStore.js
+++ vite-bookmark/src/Context/bookStore.js
@@ -3,10 +3,14 @@ import {create} from 'zustand';
 export const useBookStore = create((set) => ({
     authorized: true,
     selected_chat: null,
+    create_group_tab: false,
+    description_input: null,
     textRef: null,
     siteError: null,
     setAuthorized: (bool) => set(() => ({authorized: bool})),
     setSelectedChat: (user) => set(() => ({selected_chat: user})),
+    setCreateGroupTab: (bool) => set(() => ({create_group_tab: bool})),
+    setDescriptionInput: (text) => set(() => ({description_input: text})),
     setTextEditor: (item) => set(() => ({textRef: item})),
     setSiteError: (error) => set(() => ({siteError: error ? error : null}))
 }));
