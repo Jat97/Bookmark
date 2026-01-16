fedd4bb58cf2ef93fd79cd6145b362d8aa83eb01
Jat97 <85377239+Jat97@users.noreply.github.com>
2026-01-16 11:27:46 -0500

    Adjusted width to 150px

--- vite-bookmark/src/Routes/Buttons/EditButton.jsx
+++ vite-bookmark/src/Routes/Buttons/EditButton.jsx
@@ -2,8 +2,8 @@ import {PencilIcon} from '@heroicons/react/24/solid';
 
 const EditButton = ({save_fn}) => {
     return (
-        <button type='button' className='flex justify-around items-center text-white bg-blue-400 w-[150px] hover:bg-sky-100'
-            onClick={save_fn}> 
+        <button type='button' className='flex justify-around items-center font-semibold bg-blue-400 rounded-full 
+            w-[150px] hover:bg-sky-100' onClick={save_fn}> 
             <PencilIcon className='h-4 md:h-6' />
 
             Save changes 
