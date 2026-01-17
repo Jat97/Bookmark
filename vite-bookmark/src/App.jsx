9847759cb833a1f8184f74dba126870e0740aa60
Jat97 <85377239+Jat97@users.noreply.github.com>
2026-01-17 11:52:33 -0500

    Removed slate background and reformatted Routes so that most paths are children of Layout

--- vite-bookmark/src/App.jsx
+++ vite-bookmark/src/App.jsx
@@ -1,5 +1,6 @@
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 import {useBookStore} from './Context/bookStore';
+import Layout from './Layout';
 import Login from './Routes/Profiles/Users/LogSign/Login';
 import Signup from './Routes/Profiles/Users/LogSign/Signup';
 import Home from './Routes/Feed/Posts/Home';
@@ -8,26 +9,26 @@ import ProfilePage from './Routes/Profiles/ProfileInformation/ProfilePage';
 import FullPost from './Routes/Feed/Posts/FullPost';
 import CreateGroup from './Routes/Profiles/Groups/CreateGroup';
 import SiteErr from './Routes/Miscellaneous/Text/Errors/SiteErr';
-import Navbar from './Routes/Profiles/Users/Navbar';
 
 const App = () => {
     const create_group_tab = useBookStore((state) => state.create_group_tab);
     const error = useBookStore((state) => state.error);
     
     return (
-        <div className='bg-amber-400/25 h-full'>
-            <Navbar />
-            
+        <div className='h-full'>
             <BrowserRouter>
                 <Routes>
+                    <Route element={<Layout />}>
+                        <Route path='/api/home' element={<Home />}></Route>
+                        <Route path='/api/index/users' element={<Index />}></Route>
+                        <Route path='/api/index/groups' element={<Index />}></Route>
+                        <Route path='/api/profile/:profileid' element={<ProfilePage />}></Route>
+                        <Route path='/api/group/:profileid' element={<ProfilePage />}></Route>
+                        <Route path='/api/post/:postid/comments' element={<FullPost />}></Route>
+                    </Route>
+                    
                     <Route path='/api/login' element={<Login />}></Route>
                     <Route path='/api/signup' element={<Signup />}></Route>
-                    <Route path='/api/home' element={<Home />}></Route>
-                    <Route path='/api/index/users' element={<Index />}></Route>
-                    <Route path='/api/index/groups' element={<Index />}></Route>
-                    <Route path='/api/profile/:profileid' element={<ProfilePage />}></Route>
-                    <Route path='/api/group/:profileid' element={<ProfilePage />}></Route>
-                    <Route path='/api/post/:postid/comments' element={<FullPost />}></Route>
                     <Route path='*' element={<Navigate to='/api/login' />}></Route>
                 </Routes>
             </BrowserRouter>
