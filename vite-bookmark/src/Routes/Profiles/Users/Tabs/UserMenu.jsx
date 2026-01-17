bcbdf20a2a7570f8dc155a7adac9c570915029ed
Jat97 <85377239+Jat97@users.noreply.github.com>
2026-01-17 01:37:51 -0500

    Changed background colour to amber and hover background to yellow

--- vite-bookmark/src/Routes/Profiles/Users/Tabs/UserMenu.jsx
+++ vite-bookmark/src/Routes/Profiles/Users/Tabs/UserMenu.jsx
@@ -17,33 +17,33 @@ const UserMenu = ({user}) => {
     }
 
     return (
-        <div className='absolute flex flex-col items-center gap-1 bg-orange-300 
+        <div className='absolute flex flex-col items-center gap-1 bg-amber-300/75 
             max-h-[150px] w-[150px] z-30 md:left-[20px] md:max-h-52 md:w-[200px]'>
-            <Link to={`/api/profile/${user?.id}`} className='cursor-pointer text-center w-full hover:bg-amber-100'> 
+            <Link to={`/api/profile/${user?.id}`} className='cursor-pointer text-center w-full hover:bg-yellow-100'> 
                 Profile 
             </Link>
 
-            <Link to={`/api/user/friends/${user?.id}`} className='cursor-pointer text-center w-full hover:bg-amber-100'>
+            <Link to={`/api/profile/${user?.id}#friends`} className='cursor-pointer text-center w-full hover:bg-yellow-100'>
                 View friends
             </Link>
 
-            <Link to={`/api/user/groups/${user?.id}`} className='cursor-pointer text-center w-full hover:bg-amber-100'> 
+            <Link to={`/api/profile/${user?.id}#groups`} className='cursor-pointer text-center w-full hover:bg-yellow-100'> 
                 Groups
             </Link>
 
-            <Link to={`/api/index/users`} className='cursor-pointer text-center w-full hover:bg-amber-100'>
+            <Link to={`/api/index/users`} className='cursor-pointer text-center w-full hover:bg-yellow-100'>
                 View all users
             </Link>
 
-            <Link to={`/api/index/groups`} className='cursor-pointer text-center w-full hover:bg-amber-100'>
+            <Link to={`/api/index/groups`} className='cursor-pointer text-center w-full hover:bg-yellow-100'>
                 View all groups
             </Link>
             
-            <p className='cursor-pointer text-center w-full hover:bg-amber-100' onClick={() => enableGroupTab()}> 
+            <p className='cursor-pointer text-center w-full hover:bg-yellow-100' onClick={() => enableGroupTab()}> 
                 Create group 
             </p>
 
-            <button type='button' className='cursor-pointer w-full hover:bg-amber-100' onClick={() => endSession()}>
+            <button type='button' className='cursor-pointer w-full hover:bg-yellow-100' onClick={() => endSession()}>
                 Logout
             </button>
         </div>
