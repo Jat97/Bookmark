ca52c57a4023d32a228da08e4deb57cc69396042
Jat97 <85377239+Jat97@users.noreply.github.com>
2026-01-18 00:41:52 -0500

    Added role to div; rearranged most paragraph elements so the labels are contained in span

--- vite-bookmark/src/Routes/Profiles/ProfileInformation/ProfileInformation.jsx
+++ vite-bookmark/src/Routes/Profiles/ProfileInformation/ProfileInformation.jsx
@@ -2,20 +2,25 @@ const ProfileInformation = ({profile}) => {
     return (
         <div className='flex flex-col items-center'>
             <div>
-                <p> {profile?.date_of_birth ? 'Date of birth:' : 'Date created:'} 
-                    <span> {profile?.date_of_birth ? profile?.date_of_birth : profile?.created} </span> 
+                <p> <span className='font-semibold'> {profile.date_of_birth ? 'Date of birth:' : 'Created:'} </span> 
+                    {profile?.date_of_birth ? profile?.date_of_birth : profile?.created} 
                 </p>
 
-                {profile?.moderator && <p> Created by: <span> {`${moderator?.first_name} ${moderator?.last_name}`} </span> </p>}
+                {profile?.moderator && <p> <span className='font-semibold'> Created by: </span> 
+                    {`${moderator?.first_name} ${moderator?.last_name}`} 
+                </p>
+                }
 
                 {profile?.alma_mater && 
                     <p className='flex flex-col items-start'> Education: {profile?.alma_mater} 
                         <span className='text-sm'> {profile?.degree} </span> 
                     </p>
                 }
+
+                <p> <span className='font-semibold'> Role: </span> {profile?.role} </p>
             </div>
 
-            <p> About: <span> {profile?.description} </span> </p>
+            <p> <span className='font-semibold'> About: </span> {profile?.description} </p>
         </div>
     )
 }
