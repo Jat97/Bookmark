505ed13965faee5b13530d13f2b94004219fac91
Jat97 <85377239+Jat97@users.noreply.github.com>
2026-01-19 12:12:42 -0500

    Added AllProfiles component with accompanying Routes

--- vite-bookmark/src/App.jsx
+++ vite-bookmark/src/App.jsx
@@ -4,10 +4,12 @@ import Layout from './Layout';
 import Login from './Routes/Profiles/Users/LogSign/Login';
 import Signup from './Routes/Profiles/Users/LogSign/Signup';
 import Home from './Routes/Feed/Posts/Home';
-import Index from './Routes/Profiles/ProfileInformation/Index';
+import AllProfiles from './Routes/Profiles/ProfileInformation/AllProfiles';
 import ProfilePage from './Routes/Profiles/ProfileInformation/ProfilePage';
+import EditProfile from './Routes/Profiles/ProfileInformation/EditProfile';
 import FullPost from './Routes/Feed/Posts/FullPost';
 import CreateGroup from './Routes/Profiles/Groups/CreateGroup';
+import EditGroup from './Routes/Profiles/Groups/EditGroup';
 import SiteErr from './Routes/Miscellaneous/Text/Errors/SiteErr';
 
 const App = () => {
@@ -20,11 +22,13 @@ const App = () => {
                 <Routes>
                     <Route element={<Layout />}>
                         <Route path='/api/home' element={<Home />}></Route>
-                        <Route path='/api/index/users' element={<Index />}></Route>
-                        <Route path='/api/index/groups' element={<Index />}></Route>
+                        <Route path='/api/index/users' element={<AllProfiles />}></Route>
+                        <Route path='/api/index/groups' element={<AllProfiles />}></Route>
                         <Route path='/api/profile/:profileid' element={<ProfilePage />}></Route>
                         <Route path='/api/group/:profileid' element={<ProfilePage />}></Route>
                         <Route path='/api/post/:postid/comments' element={<FullPost />}></Route>
+                        <Route path='/api/profile/edit' element={<EditProfile />}></Route>
+                        <Route path='/api/group/edit' element={<EditGroup />}></Route>
                     </Route>
                     
                     <Route path='/api/login' element={<Login />}></Route>
