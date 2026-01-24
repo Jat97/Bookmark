854e97464b1c20a94658c606af772e3922f37b3d
Jat97 <85377239+Jat97@users.noreply.github.com>
2026-01-23 23:19:15 -0500

    Adjusted height of PencilIcon to be h-5 when viewport is medium

--- vite-bookmark/src/Routes/Buttons/Profile/EditButton.jsx
+++ vite-bookmark/src/Routes/Buttons/Profile/EditButton.jsx
@@ -2,9 +2,9 @@ import {PencilIcon} from '@heroicons/react/24/solid';
 
 const EditButton = ({save_fn}) => {
     return (
-        <button type='button' className='flex justify-around items-center font-semibold bg-blue-400 rounded-full 
-            w-[150px] hover:bg-sky-100' onClick={save_fn}> 
-            <PencilIcon className='h-4 md:h-6' />
+        <button type='button' className='cursor-pointer flex justify-around items-center font-semibold bg-blue-400 
+            rounded-full w-[150px] hover:bg-sky-100' onClick={save_fn}> 
+            <PencilIcon className='h-4 md:h-5' />
 
             Save changes 
         </button>
