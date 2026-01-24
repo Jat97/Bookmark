c58bf0c795fed96c2529c71288ab9c0eee36878c
Jat97 <85377239+Jat97@users.noreply.github.com>
2026-01-23 23:29:35 -0500

    Adjusted width

--- vite-bookmark/src/Routes/Miscellaneous/Inputs/UserSelectInput.jsx
+++ vite-bookmark/src/Routes/Miscellaneous/Inputs/UserSelectInput.jsx
@@ -1,8 +1,8 @@
 const UserSelectInput = ({value, select_fn}) => {
     return (
-        <select id='role' className='border-solid border-slate-200 bg-slate-200 rounded-2xl p-1 w-[150px]' 
+        <select id='role' className='border-solid border-slate-200 bg-slate-200 rounded-2xl p-1 w-[175px]' 
             {...(select_fn && {value: value, onChange: select_fn})}>
-            <option value='Default'> Select a role </option>
+            <option value=''> Select a role </option>
             <option value='Reader'> Reader </option>
             <option value='Essayist'> Essayist </option>
             <option value='Poet'> Poet </option>
