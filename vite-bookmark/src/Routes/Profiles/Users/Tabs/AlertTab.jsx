6c694193c786b30a31b823ba23acfc6e44d96390
Jat97 <85377239+Jat97@users.noreply.github.com>
2026-01-24 00:17:46 -0500

    Adjusted width, height, and positioning; removed conditional to render components, instead rendering ProfileDispaly only once and using conditionals to handle what renders after it

--- vite-bookmark/src/Routes/Profiles/Users/Tabs/AlertTab.jsx
+++ vite-bookmark/src/Routes/Profiles/Users/Tabs/AlertTab.jsx
@@ -13,28 +13,28 @@ const AlertTab = ({alerts}) => {
 
     return (
         <div className='absolute border border-slate-200 bg-slate-200/50 max-h-screen max-w-screen 
-            md:left-[115px] md:bg-orange-200 md:max-h-60 md:max-w-32'>
+            md:top-[30px] md:left-[-25px] md:bg-amber-300/50 md:max-h-80 md:w-[250px]'>
             {alertView &&
                 <XMarkIcon className='h-6 fill-slate-400 md:hidden hover:fill-zinc-100' onClick={() => toggleAlertView()}/> 
             }
 
             <div>
                {alerts?.map(alert => {
-                    if(alert.requesting_user) {
-                        return (
+                    <div className={`flex items-center ${alert.requesting_user ? 'justify-around' : 'flex-col'}`}>
+                        <ProfileDisplay 
+                        profile={alert.requesting_user ? alert.requesting_user : alert.alerting_user ? alert.alerting_user : alert.alerting_group} 
+                        is_logged={false}
+                        profile_mode={'index'} />
+
+                        {alert.requesting_user &&
                             <div className='flex justify-around items-center'>
-                               <ProfileDisplay props={[alert.requesting_user, false, 'index']} />
+                                <AcceptButton props={alert.requesting_user} />
 
-                               <div className='flex justify-around items-center'>
-                                    <AcceptButton props={alert.requesting_user} />
+                                <RejectButton props={alert.requesting_user} /> 
+                            </div>
+                        }
 
-                                    <RejectButton props={alert.requesting_user} /> 
-                               </div>
-                            </div>   
-                        )
-                    }
-                    else {
-                        return (
+                        {(alert.alerting_user || alert.alerting_group) &&
                             <div className='flex flex-col items-start'>
                                 <ProfileDisplay props={[
                                     alert.alerting_user ? alert.alerting_user : alert.alerting_group,
@@ -47,13 +47,13 @@ const AlertTab = ({alerts}) => {
                                     <p className='text-xs text-gray-200'> {alert?.sent} </p>
                                 </div>
                             </div>
-                        )
-                    }
+                        }
+                    </div>
                })}  
             </div>
 
             {!alertView &&
-                <div> View all alerts </div>
+                <p> View all alerts </p>
             }
         </div>
     )
