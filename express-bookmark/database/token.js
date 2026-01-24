7ac61b336640f5df9bfc5fdeffa4a03f54e2c14b
Jat97 <85377239+Jat97@users.noreply.github.com>
2026-01-23 23:16:39 -0500

    Removed req.cookies.signtoken

--- express-bookmark/database/token.js
+++ express-bookmark/database/token.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports.validateToken = (req, res) => {
     return new Promise((resolve, reject) => {
-        const token = req.cookies.usertoken || req.cookies.signtoken;
+        const token = req.cookies.usertoken;
 
         if(token) {
             jwt.verify(token, process.env.TOKEN_KEY, (err, key) => {
