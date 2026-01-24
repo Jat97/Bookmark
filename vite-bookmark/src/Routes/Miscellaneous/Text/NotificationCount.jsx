4f1660659d438a00bcfcdeba7fbaeb99ebea210d
Jat97 <85377239+Jat97@users.noreply.github.com>
2026-01-23 23:41:39 -0500

    Added styling, including absolute positioning and width

--- vite-bookmark/src/Routes/Miscellaneous/Text/NotificationCount.jsx
+++ vite-bookmark/src/Routes/Miscellaneous/Text/NotificationCount.jsx
@@ -1,6 +1,8 @@
 const NotificationCount = ({count}) => {
     return (
-        <div className={`${count > 0 ? 'font-semibold bg-red-400 text-white rounded-full w-1' : 'hidden'}`}> {count} </div>
+        <div className={`${count > 0 ? 'absolute top-[5px] left-[20px] font-semibold bg-red-400 text-white rounded-full w-[10px]' : 'hidden'}`}> 
+            {count} 
+        </div>
     )
 };
 
