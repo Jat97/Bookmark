9727dad0d10fa67eada6de8cab3c01746f38154d
Jat97 <85377239+Jat97@users.noreply.github.com>
2026-01-23 23:18:25 -0500

    Adjusted width; added white strokes to TrashIcon

--- vite-bookmark/src/Routes/Buttons/Profile/DeleteButton.jsx
+++ vite-bookmark/src/Routes/Buttons/Profile/DeleteButton.jsx
@@ -2,9 +2,10 @@ import {TrashIcon} from '@heroicons/react/24/solid';
 
 const DeleteButton = ({delete_fn}) => {
     return (
-        <button type='button' className='flex justify-around items-center bg-red-400 hover:bg-pink-100'
+        <button type='button' className='cursor-pointer flex justify-around items-center bg-red-400 rounded-full 
+            w-[120px] hover:bg-pink-100'
             onClick={delete_fn}>
-            <TrashIcon className='h-4 stroke-white md:h-6' />
+            <TrashIcon className='h-4 stroke-white md:h-5' />
 
             <span className='text-white'> Delete </span>
         </button>
