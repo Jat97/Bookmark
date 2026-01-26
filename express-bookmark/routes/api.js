c3ac1d8ed1f85ab39cff4130644d82ec850169cf
Jat97 <85377239+Jat97@users.noreply.github.com>
2026-01-26 00:09:48 -0500

    Edited endpoints to both simplify them and make them more RESTful

--- express-bookmark/routes/api.js
+++ express-bookmark/routes/api.js
@@ -18,27 +18,27 @@ router.get('/users', userController.get_all_users);
 
 router.get('/blocked', userController.get_blocked_list);
 
-router.post('/block/:userid', userController.block_user);
+router.post('/:userid/block', userController.block_user);
 
-router.delete('/unblock/:userid', userController.unblock_user);
+router.delete('/:userid/unblock', userController.unblock_user);
 
 router.get('/friends', userController.get_friends_list);
 
-router.delete('/unfriend/:userid', userController.remove_from_friendslist);
+router.delete('/:userid/unfriend', userController.remove_from_friendslist);
 
 router.get('/notifications', userController.get_notifications);
 
-router.post('/request/:userid', userController.send_friend_request);
+router.post('/:userid/request', userController.send_friend_request);
 
-router.post('/request/accept/:userid', userController.accept_friend_request);
+router.post('/:userid/accept', userController.accept_friend_request);
 
-router.delete('/request/reject/:userid', userController.reject_friend_request);
+router.delete('/:userid/reject', userController.reject_friend_request);
 
 router.get('/user', userController.get_logged_information);
 
 router.patch('/user/picture', upload.single('profilepic'), userController.edit_profile_picture);
 
-router.put('/user/profile/edit', userController.edit_profile_information);
+router.put('/user', userController.edit_profile_information);
 
 router.patch('/user/hidden', userController.update_hidden_status);
 
@@ -50,25 +50,25 @@ router.delete('/user', userController.delete_account);
 
 router.get('/groups', groupController.get_all_groups);
 
-router.post('/group/create', groupController.create_group);
+router.post('/group', groupController.create_group);
 
-router.put('/group/update/:groupid', groupController.update_group_information);
+router.put('/:groupid', groupController.update_group_information);
 
-router.patch('/group/private/:groupid', groupController.handle_group_privacy);
+router.patch('/:groupid/private', groupController.handle_group_privacy);
 
-router.post('/group/request/:groupid', groupController.send_group_request);
+router.post('/:groupid/request', groupController.send_group_request);
 
-router.delete('/group/leave/:groupid', groupController.leave_group);
+router.delete('/:groupid/leave', groupController.leave_group);
 
-router.post('/group/:groupid/request/accept/:userid', groupController.accept_group_request);
+router.post('/:groupid/:userid/accept', groupController.accept_group_request);
 
-router.delete('/group/:groupid/request/reject/:userid', groupController.reject_group_request);
+router.delete('/:groupid/:userid/reject', groupController.reject_group_request);
 
-router.post('/group/:groupid/ban/:userid', groupController.ban_user);
+router.post('/:groupid/:userid/ban', groupController.ban_user);
 
-router.delete('/group/:groupid/unban/:userid', groupController.unban_user);
+router.delete('/:groupid/:userid/unban', groupController.unban_user);
 
-router.delete('/group/:groupid', groupController.delete_group);
+router.delete('/:groupid', groupController.delete_group);
 
 // Post routes
 
@@ -76,31 +76,29 @@ router.get('/posts', postController.get_post_feed);
 
 router.post('/post', upload.array('postimage', 10), postController.create_post);
 
-router.put('/post/edit/:postid', upload.array('postimage', 10), postController.edit_post);
+router.put('/:postid', upload.array('postimage', 10), postController.edit_post);
 
-router.post('/post/like/:postid', postController.like_post);
+router.post('/:postid/like', postController.like_post);
 
-router.delete('/post/unlike/:postid', postController.unlike_post);
+router.delete('/:postid/unlike', postController.unlike_post);
 
-router.post('/post/share/:postid', postController.share_post);
+router.post('/:postid/share', postController.share_post);
 
-router.delete('/post/:postid', postController.delete_post);
+router.delete('/:postid', postController.delete_post);
 
 // Comment routes
 
-router.get('/post/:postid/comments', commentController.view_post_comments);
+router.get('/:postid/comments', commentController.view_post_comments);
 
-router.post('/post/comment/:postid', upload.single('commentimage'), commentController.create_parent_comment);
+router.post('/:postid/comment', upload.single('commentimage'), commentController.create_comment);
 
-router.post('/comment/reply/:commentid', upload.single('commentimage'), commentController.reply_to_comment);
+router.put('/:commentid', commentController.edit_comment);
 
-router.put('/comment/edit/:commentid', commentController.edit_comment);
+router.delete('/:commentid', commentController.delete_comment);
 
-router.delete('/comment/:commentid', commentController.delete_comment);
+router.post('/:commentid/like', commentController.like_comment);
 
-router.post('/comment/like/:commentid', commentController.like_comment);
-
-router.delete('/comment/unlike/:commentid', commentController.unlike_comment);
+router.delete('/:commentid/unlike', commentController.unlike_comment);
 
 // Chat routes
 
@@ -108,6 +106,6 @@ router.get('/chats', chatController.get_all_chats);
 
 router.get('/message/:userid', upload.single('chatimage'), chatController.send_message);
 
-router.delete('/chat/:chatid', chatController.delete_chat);
+router.delete('/:chatid', chatController.delete_chat);
 
 module.exports = router;
