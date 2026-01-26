cadbbf3b1383103fb919bb5f30fd62e9bf36018f
Jat97 <85377239+Jat97@users.noreply.github.com>
2026-01-26 00:30:03 -0500

    Edited logged prop in Index call to pass loggedData to component

--- vite-bookmark/src/Routes/Profiles/ProfileInformation/AllProfiles.jsx
+++ vite-bookmark/src/Routes/Profiles/ProfileInformation/AllProfiles.jsx
@@ -1,6 +1,6 @@
 import {useLocation} from 'react-router-dom';
 import {useBookStore} from '../../../Context/bookStore';
-import {useFetchUsers} from '../../Functions/Queries/UserQueries';
+import {useFetchLogged, useFetchUsers} from '../../Functions/Queries/UserQueries';
 import {useFetchGroups} from '../../Functions/Queries/GroupQueries';
 import Index from './Index';
 import PageHeader from '../../Miscellaneous/Text/PageHeader';
@@ -12,6 +12,7 @@ const AllProfiles = () => {
     const setAuthorized = useBookStore((state) => state.setAuthorized);
     const setSiteError = useBookStore((state) => state.setSiteError);
 
+    const loggedData = useFetchLogged([authorized, setAuthorized, setSiteError]);
     const userData = useFetchUsers([authorized, setAuthorized, setSiteError]);
     const groupData = useFetchGroups([authorized, setAuthorized, setSiteError]);
 
@@ -20,9 +21,8 @@ const AllProfiles = () => {
             <PageHeader header={`${location.pathname.includes('groups') ? 'Groups' : 'Users'}`} />
 
             <Index 
-                logged={null} 
-                moderator={null} 
-                items={location.pathname.includes('groups') ? groupData.data.groups : userData.data.users} 
+                logged={loggedData.data.logged_user.profile} 
+                items={location.pathname.includes('groups') ? groupData.data?.groups : userData.data?.users} 
             />
         </div>
     )
