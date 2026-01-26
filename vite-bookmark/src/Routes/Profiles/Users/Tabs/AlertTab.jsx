0159e9f11cdf51d0ee109f293646c10622850a2b
Jat97 <85377239+Jat97@users.noreply.github.com>
2026-01-26 00:57:47 -0500

    Changed onClick attribute on XMarkIcon so it only binds when alertView is true; styled View all alerts p element and added toggleAlertView to onClick

--- vite-bookmark/src/Routes/Profiles/Users/Tabs/AlertTab.jsx
+++ vite-bookmark/src/Routes/Profiles/Users/Tabs/AlertTab.jsx
@@ -2,6 +2,7 @@ import {useState} from 'react';
 import AcceptButton from '../../../Buttons/Profile/AcceptButton';
 import RejectButton from '../../../Buttons/Profile/RejectButton';
 import ProfileDisplay from '../../ProfileInformation/ProfileDisplay';
+import NoItems from '../../../Miscellaneous/Text/NoItems';
 import {XMarkIcon} from '@heroicons/react/24/solid';
 
 const AlertTab = ({alerts}) => {
@@ -12,48 +13,53 @@ const AlertTab = ({alerts}) => {
     }
 
     return (
-        <div className='absolute border border-slate-200 bg-slate-200/50 max-h-screen max-w-screen 
-            md:top-[30px] md:left-[-25px] md:bg-amber-300/50 md:max-h-80 md:w-[250px]'>
+        <div className={`absolute flex flex-col items-center bg-slate-200
+            max-h-screen max-w-screen md:bg-yellow-300 md:top-[30px] 
+                md:left-[-100px] md:gap-3 md:max-h-80 md:w-[250px]
+            ${alertView && 'bg-slate-200/50'}`}>
             {alertView &&
-                <XMarkIcon className='h-6 fill-slate-400 md:hidden hover:fill-zinc-100' onClick={() => toggleAlertView()}/> 
+                <XMarkIcon className='h-6 fill-slate-400 md:hidden hover:fill-zinc-100' 
+                    {...(alertView && {onClick: toggleAlertView})}/> 
             }
 
-            <div>
+            {alerts.length === 0 &&
+                <NoItems text={`There's nothing here.`} />
+            }
+
+            <div className={`${alertView && 'border border-slate-200'}`}>
                {alerts?.map(alert => {
-                    <div className={`flex items-center ${alert.requesting_user ? 'justify-around' : 'flex-col'}`}>
-                        <ProfileDisplay 
-                        profile={alert.requesting_user ? alert.requesting_user : alert.alerting_user ? alert.alerting_user : alert.alerting_group} 
-                        is_logged={false}
-                        profile_mode={'index'} />
+                    return (
+                        <div className='flex flex-col items-center'>
+                            <ProfileDisplay 
+                            profile={alert} 
+                            is_logged={false}
+                            profile_mode={'index'} />
 
-                        {alert.requesting_user &&
-                            <div className='flex justify-around items-center'>
-                                <AcceptButton props={alert.requesting_user} />
+                            {alert.first_name &&
+                                <div className='flex justify-around items-center gap-x-2'>
+                                    <AcceptButton user_group={alert} />
 
-                                <RejectButton props={alert.requesting_user} /> 
-                            </div>
-                        }
-
-                        {(alert.alerting_user || alert.alerting_group) &&
-                            <div className='flex flex-col items-start'>
-                                <ProfileDisplay props={[
-                                    alert.alerting_user ? alert.alerting_user : alert.alerting_group,
-                                    false,
-                                    'index'
-                                    ]} />
-
-                                <div className='flex justify-between items-center'>
-                                    <p className='text-sm'> {alert?.text} </p>
-                                    <p className='text-xs text-gray-200'> {alert?.sent} </p>
+                                    <RejectButton user_group={alert} /> 
                                 </div>
-                            </div>
-                        }
-                    </div>
+                            }
+
+                            {alert.text &&
+                                <div className='flex flex-col items-start'>
+                                    <div className='flex justify-between items-center'>
+                                        <p className='text-sm'> {alert?.text} </p>
+                                        <p className='text-xs text-gray-200'> {alert?.sent} </p>
+                                    </div>
+                                </div>
+                            }
+                        </div>
+                    )
                })}  
             </div>
 
-            {!alertView &&
-                <p> View all alerts </p>
+            {(!alertView && alerts.length > 0) &&
+                <p className='cursor-pointer text-blue-600 hover:underline' onClick={() => toggleAlertView()}> 
+                    View all alerts 
+                </p>
             }
         </div>
     )
