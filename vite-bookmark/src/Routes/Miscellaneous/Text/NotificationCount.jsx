015285c840dc137148b054a885eebb43f58b29bf
Jat97 <85377239+Jat97@users.noreply.github.com>
2026-04-10 01:49:25 -0400

    Adjusted component to better fit with the parent div

--- vite-bookmark/src/Routes/Miscellaneous/Text/NotificationCount.jsx
+++ vite-bookmark/src/Routes/Miscellaneous/Text/NotificationCount.jsx
@@ -1,6 +1,7 @@
 const NotificationCount = ({count}) => {
     return (
-        <div className={`${count > 0 ? 'absolute top-[5px] left-[20px] font-semibold bg-red-400 text-white rounded-full w-[10px]' : 'hidden'}`}> 
+        <div className={`${count > 0 ? `absolute text-center top-[20px] left-[110px] font-semibold bg-red-500/50 text-white 
+            rounded-full w-[20px] md:top-[5px] md:left-[20px]` : 'hidden'}`}> 
             {count} 
         </div>
     )
