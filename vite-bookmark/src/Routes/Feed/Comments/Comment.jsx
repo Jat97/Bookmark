5396d2b47547bf38bde3487e104fbda62bc2415c
Jat97 <85377239+Jat97@users.noreply.github.com>
2026-04-10 01:21:35 -0400

    Replaced p element with span

--- vite-bookmark/src/Routes/Feed/Comments/Comment.jsx
+++ vite-bookmark/src/Routes/Feed/Comments/Comment.jsx
@@ -1,22 +1,33 @@
-import ProfileDisplay from '../../Profiles/ProfileInformation/ProfileDisplay';
-import PostCommentBox from '../Posts/PostCommentBox';
+import {TrashIcon} from '@heroicons/react/24/solid';
+import {useDeleteCommentMutation} from '../../Functions/Mutations/PostMutations';
+import {useBookStore} from '../../../Context/bookStore';
+import ProfileDisplay from '../../Profiles/ProfileInformation/Accounts/ProfileDisplay';
+import PostCommentBox from '../PostCommentBox';
 
-const Comment = ({comment}) => {
+const Comment = ({comment, is_logged}) => {
+    const setSiteError = useBookStore((state) => state.setSiteError);
+
+    const delete_comment_mutation = useDeleteCommentMutation([comment?.id, setSiteError]);
+
+    const deleteComment = () => {
+        return delete_comment_mutation.mutate();
+    }
+
     return (
-        <div className='flex flex-col items-start'>
-            <div>
-                <ProfileDisplay profile={comment.commenting_user ? comment.commenting_user : comment.commenting_group} 
-                    is_logged={false} profile_mode={'post'}
-                />
-            </div>
+        <div className='relative flex flex-col items-start'>
+            {is_logged &&
+                <TrashIcon className='absolute top-[5px] right-[10px] cursor-pointer h-4 stroke-zinc-300 
+                    md:h-5 hover:stroke-slate-100' 
+                    onClick={() => deleteComment()} />
+            }
             
-            <div className='max-w-3/4'>
-                {comment.text}
-            </div>
+            <ProfileDisplay profile={comment?.commenting_user ? comment?.commenting_user : comment?.commenting_group} 
+                is_logged={false} profile_mode={'post'}
+            />
+            
+            <span className='m-1 p-1 break-normal w-3/4' dangerouslySetInnerHTML={{__html: comment?.text}}></span>
 
-            <div>
-                <PostCommentBox post={comment} />
-            </div>
+            <PostCommentBox post={comment} />
         </div>
     )
 }
