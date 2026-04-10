73079a1f4e6f1fa7f8018fb8bd8d29e8c14c74a4
Jat97 <85377239+Jat97@users.noreply.github.com>
2026-04-10 01:59:12 -0400

    Added object parameter to mutation call in createGroup

--- vite-bookmark/src/Routes/Profiles/Groups/CreateGroup.jsx
+++ vite-bookmark/src/Routes/Profiles/Groups/CreateGroup.jsx
@@ -1,7 +1,11 @@
 import {useState} from 'react';
 import {useNavigate} from 'react-router-dom';
-import {XMarkIcon} from '@heroicons/react/24/solid';
+import {EyeIcon, EyeSlashIcon} from '@heroicons/react/24/solid';
 import {useBookStore} from '../../../Context/bookStore';
+import {useFetchUsers} from '../../Functions/Queries/UserQueries';
+import {useFetchGroups} from '../../Functions/Queries/GroupQueries';
+import {useCreateGroupMutation} from '../../Functions/Mutations/GroupMutations';
+import ProfileBox from '../ProfileInformation/Accounts/ProfileBox';
 import UserGroupInput from '../../Miscellaneous/Inputs/UserGroupInput';
 import DescriptionBox from '../../Miscellaneous/Inputs/DescriptionBox';
 import InputErr from '../../Miscellaneous/Text/Errors/InputErr';
@@ -13,64 +17,76 @@ const CreateGroup = () => {
         description: null
     });
 
-    const setCreateGroupTab = useBookStore((state) => state.setCreateGroupTab);
+    const [groupInputs, setGroupInputs] = useState({
+        title: '',
+        description: '',
+        private: false
+    });
+    
+    const authorized = useBookStore((state) => state.authorized);
+    const setAuthorized = useBookStore((state) => state.setAuthorized);
     const setSiteError = useBookStore((state) => state.setSiteError);
+
+    const userData = useFetchUsers([authorized, setAuthorized, setSiteError]);
+    const groupData = useFetchGroups([authorized, setAuthorized, setSiteError]);
 
     const navigate = useNavigate();
 
-    const disableCreateGroupTab = () => {
-        setCreateGroupTab(false);
+    const create_group_mutation = useCreateGroupMutation([navigate, setGroupErrors, setSiteError]);
+
+    const handleTextInputs = (e) => {
+        setGroupInputs((prevState) => ({
+            ...prevState,
+            [e.target.id]: e.target.value
+        }));
+    }
+
+    const selectPrivacy = (e) => {
+        setGroupInputs((prevState) => ({
+            ...prevState,
+            private: e.target.value === 'private' && true
+        }));
     }
 
     const createGroup = () => {
-        const title = document.querySelector('#title').value;
-        const description = document.querySelector('#description').value
-
-        fetch('http://localhost:9000/api/group', {
-            method: 'POST',
-            credentials: 'include',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                title: title,
-                description: description
-            }) 
-        })
-        .then(res => {
-            if(!res.ok) {
-                return res.json();
-            }
-            else if(res.redirected) {
-                return navigate(res.url.replace('9000', '3000'), {rewrite: true});
-            }
-        })
-        .then(json => {
-            json.errors.errors.forEach(error => {
-                setGroupErrors({
-                    title: error.title ? error.title : null,
-                    description: error.description ? error.description : null
-                });
-            })
-        })
-        .catch(err => setSiteError(err.message))
-    };
+        return create_group_mutation.mutate({group: groupInputs});
+    }
 
     return (
-        <div className='absolute top-0 right-0 bg-slate-200/50 h-screen w-screen z-50'>
-            <XMarkIcon className='absolute top-[10px] right-[10px] stroke-slate-400 md:h-8 hover:stroke-zinc-100' 
-                onClick={() => disableCreateGroupTab()}
-            />
-
-            <div className='relative top-[100px] left-[350px] flex flex-col items-center gap-[20px] bg-white border-slate-200 w-1/2'> 
+        <div className='flex justify-between items-start h-screen w-screen m-2 z-50'>
+            <div className='flex flex-col items-center gap-[20px]'> 
                 <PageHeader header={'Start a new group!'} />
 
                 <div>
+                    <div className='flex flex-col items-start gap-y-5'>
+                        <label htmlFor='public' className='flex justify-around items-start gap-x-10'>
+                            <input type='radio' id='public' value='public' checked={!groupInputs.private && true}
+                                onChange={(e) => selectPrivacy(e)}></input>
+
+                            <EyeIcon className='h-4 stroke-slate-300 md:h-6' />
+
+                            <span className='flex flex-col items-start text-slate-300'> Public 
+                                <span className='text-sm'> Anyone can participate </span>
+                            </span>
+                        </label>
+
+                        <label htmlFor='private' className='flex justify-around items-start gap-x-10'>
+                            <input type='radio' id='private' value='private' checked={groupInputs.private && true}
+                                onChange={(e) => selectPrivacy(e)}></input>
+
+                            <EyeSlashIcon className='h-4 stroke-slate-300 md:h-6' />
+
+                            <span className='text-slate-300 flex flex-col items-start'> Private 
+                                <span className='text-sm'> Only approved users can participate </span>
+                            </span>
+                        </label>
+                    </div>
+
                     <div className='flex flex-col items-start mb-[20px]'>
-                        <label for='title' className='flex flex-col items-start'>
+                        <label htmlFor='title' className='flex flex-col items-start'>
                             <span className='font-semibold'> Title </span>
 
-                            <UserGroupInput id={'title'} input_value={''} input_fn={null} />
+                            <UserGroupInput id={'title'} input_value={groupInputs.title} input_fn={handleTextInputs} />
                         </label>  
 
                         {groupErrors.title &&
@@ -79,7 +95,7 @@ const CreateGroup = () => {
                     </div>
                     
                     <div className='flex flex-col items-start mb-[10px]'>
-                        <DescriptionBox description={''} editDescription={null} is_user={false} />
+                        <DescriptionBox description={groupInputs.description} editDescription={handleTextInputs} is_user={false} />
 
                         {groupErrors.description &&
                             <InputErr error={groupErrors.description} />
@@ -87,10 +103,17 @@ const CreateGroup = () => {
                     </div>
                 </div> 
 
-                <button type='button' className='font-semibold bg-fuchsia-400 rounded-full p-1 mb-[10px] hover:bg-pink-100 md:w-[150px]' 
+                <button type='button' className='font-semibold bg-fuchsia-400 rounded-full p-1 mb-[10px] w-[150px]
+                    hover:bg-pink-100' 
                     onClick={() => createGroup()}>
                     Create group
                 </button>
+            </div>
+
+            <div className='hidden md:flex md:flex-col md:items-center md:gap-y-10'>
+                <ProfileBox title='Users' profile={null} items={userData.data?.users} />
+
+                <ProfileBox title='Groups' profile={null} items={groupData.data?.groups} />
             </div>
         </div>
     )
