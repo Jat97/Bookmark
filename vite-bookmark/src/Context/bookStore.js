dbb87157ddca7a2b11eb7eecfb8320bd0fee328a
Jat97 <85377239+Jat97@users.noreply.github.com>
2026-04-10 00:35:25 -0400

    Added guest state and storage to keep state consistent across refrefreshes

--- vite-bookmark/src/Context/bookStore.js
+++ vite-bookmark/src/Context/bookStore.js
@@ -1,14 +1,29 @@
 import {create} from 'zustand';
+import {persist, createJSONStorage} from 'zustand/middleware';
 
-export const useBookStore = create((set) => ({
-    authorized: true,
-    selected_chat: null,
-    create_group_tab: false,
-    textRef: null,
-    siteError: null,
-    setAuthorized: (bool) => set(() => ({authorized: bool})),
-    setSelectedChat: (user) => set(() => ({selected_chat: user})),
-    setCreateGroupTab: (bool) => set(() => ({create_group_tab: bool})),
-    setTextEditor: (item) => set(() => ({textRef: item})),
-    setSiteError: (error) => set(() => ({siteError: error ? error : null}))
-}));
+export const useBookStore = create(
+    persist((set) => ({
+        is_guest: false,
+        authorized: true,
+        selected_chat: null,
+        create_group_tab: false,
+        popup: false,
+        textRef: null,
+        siteError: null,
+        mobileView: window.innerWidth < 768,
+        setGuest: (bool) => set({is_guest: bool}),  
+        setAuthorized: (bool) => set(() => ({authorized: bool})),
+        setSelectedChat: (user) => set(() => ({selected_chat: user})),
+        setCreateGroupTab: (bool) => set(() => ({create_group_tab: bool})),
+        setPopup: (bool) => set(() => ({popup: bool})), 
+        setTextEditor: (item) => set(() => ({textRef: item})),
+        setSiteError: (error) => set(() => ({siteError: error ? error : null}))
+    }),
+    {
+        name: 'bookStorage',
+        partialize: (state) => ({
+            is_guest: state.is_guest
+        }),
+        storage: createJSONStorage(() => localStorage)
+    })
+);
