5b815a4b7342e60d7d7bb48649e6e9b20b4502f8
Jat97 <85377239+Jat97@users.noreply.github.com>
2026-04-10 01:05:01 -0400

    Removed conditional involving message send times; replaced p elements with spans

--- vite-bookmark/src/Routes/Chats/Message.jsx
+++ vite-bookmark/src/Routes/Chats/Message.jsx
@@ -2,19 +2,22 @@ import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 dayjs.extend(relativeTime);
 
-const Message = ({message, is_logged}) => {
+const Message = ({message, chat, is_logged}) => {
     return (
-        <div className={`flex flex-col ${is_logged ? 'items-start' : 'items-end'}`}>
+        <div className={`flex flex-col w-full p-1 ${is_logged ? 'items-end' : 'items-start'}`}>
             {message.image &&
-                <img src={message.image}></img>
+                <img src={message.image} className='w-[150px]'></img>
             }
 
-            <div className={`rounded-tl-xs rounded-tr-xs 
-                ${is_logged ? 'rounded-br-xl rounded-bl-xs bg-amber-400' : 'rounded-br-xs rounded-bl-xl'}`}>
+            <div className={`word-break rounded-tl-md rounded-tr-md w-fit
+                ${is_logged ? 'rounded-br-xs rounded-bl-md bg-amber-400' : 'rounded-br-md rounded-bl-xs bg-stone-400'}
+                ${message.text && 'p-1' }`}>
                 <p> {message.text} </p>
             </div>
 
-            <p className='text-xs text-slate-200'> {dayjs.to(message.sent)} </p>
+            {message.sent === chat.last_message_sent && 
+                <p className='text-xs text-slate-200'> {dayjs().to(message.sent)} </p>
+            }
         </div>
     )
 }
