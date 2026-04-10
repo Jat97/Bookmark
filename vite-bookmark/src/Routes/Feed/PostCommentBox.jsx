89ab96a37208545d4b193b12345003e0e4376be9
Jat97 <85377239+Jat97@users.noreply.github.com>
2026-04-10 01:33:24 -0400

    Added conditional to control what gets displayed when is_guest is true

--- vite-bookmark/src/Routes/Feed/PostCommentBox.jsx
+++ vite-bookmark/src/Routes/Feed/PostCommentBox.jsx
@@ -1,6 +1,6 @@
 import {useState} from 'react';
 import {Link} from 'react-router-dom';
-import {ArrowUturnLeftIcon, ChatBubbleLeftRightIcon} from "@heroicons/react/24/solid";
+import {ArrowUturnLeftIcon, ChatBubbleLeftRightIcon, ChatBubbleOvalLeftIcon, HandThumbUpIcon} from "@heroicons/react/24/solid";
 import {useBookStore} from '../../Context/bookStore';
 import {useFetchLogged} from '../Functions/Queries/UserQueries';
 import {useSharePostMutation} from '../Functions/Mutations/PostMutations';
@@ -8,17 +8,17 @@ import LikeButton from '../Buttons/Post/LikeButton';
 import TextBox from '../Miscellaneous/Inputs/TextBox';
 
 const PostCommentBox = ({post}) => {
-    const [displayTextBox, setDisplayTextBox] = useState(false);
+    const [textDisplay, setTextDisplay] = useState(false);
 
     const authorized = useBookStore((state) => state.authorized);
     const setAuthorized = useBookStore((state) => state.setAuthorized);
     const setSiteError = useBookStore((state) => state.setSiteError);
 
-    const loggedData = useFetchLogged([authorized, setAuthorized, setSiteError]);
-    const shareMutation = useSharePostMutation([post.id, setSiteError]);
+    const loggedData = !is_guest && useFetchLogged([authorized, setAuthorized, setSiteError]);
+    const shareMutation = useSharePostMutation([post?.id, setSiteError]);
 
     const toggleTextBox = () => {
-        setDisplayTextBox(displayTextBox ? false : true);
+        setTextDisplay(textDisplay ? false : true);
     }
 
     const sharePost = () => {
@@ -26,49 +26,56 @@ const PostCommentBox = ({post}) => {
     }
 
     return (
-        <div className='bg-amber-300/50 p-2 w-full'>
-            {post.likes.length === 0 ?
-                null
-            :
-                <div className='flex justify-evenly items-center'>
+        <div className='flex flex-col items-start gap-3 bg-orange-700/50 p-2 w-full'>
+            {post?.likes?.length > 0 &&
+                <div className='flex justify-start items-center gap-x-3 border-b-1 border-slate-100 border-solid 
+                    p-1 w-full'>
+                    <HandThumbUpIcon className='h-4 fill-amber-300 md:h-6' /> 
+                    
                     {post.likes.slice(0, 3).map(like => {
-                        return (
-                            <Link to={`/api/users/${like.id}`} className='text-blue-600 hover:underline'> 
-                                {`${like.first_name} ${like.last_name}`} 
+                        return <Link to={`/api/users/${like.id}`} className='font-semibold text-sky-600 hover:underline'> 
+                                {`${like?.first_name} ${like.last_name}`} 
                             </Link>
-                        )
                     })}
 
-                    {post.likes.length > 3 &&
-                        <p> and <span className='text-blue-600 hover:underline'> {post.likes.length} others </span> </p>
-                    }
+                    {post.likes.length > 3 && <span> and {post.likes?.length} others </span>}
+                </div>
+            } 
+            
+            {!is_guest && 
+                <div className={`text-amber-300 grid items-center md:w-full 
+                        ${post.commenting_user ? 'grid-cols-2' : 'grid-cols-3'}`}>
+                    <LikeButton logged={loggedData.data?.profile} post={post} />
 
-                    liked this
+                    <button type='button' className={`cursor-pointer flex justify-center items-center gap-x-2 
+                        w-[75px] p-1 md:w-[150px] ${post.commenting_user && 'hidden'}`} 
+                        onClick={() => sharePost()}>
+                        <ArrowUturnLeftIcon className='h-4 md:h-6' />
+
+                        Share
+                    </button>
+
+                    {post?.commenting_user || post?.commenting_group ?
+                        <button type='button' className='cursor-pointer flex justify-center items-center gap-x-2
+                            text-sm w-[125px] md:text-base md:w-[150px]' onClick={() => toggleTextBox()}>
+                            <ChatBubbleOvalLeftIcon className='h-4 stroke-amber-300 md:h-6' />
+
+                            Comment
+                        </button>
+                    :
+                        <Link to={`/api/post/${post?.id}/comments`} className='cursor-pointer flex justify-center items-center 
+                            gap-x-2 text-sm p-1 w-[125px] md:text-base md:w-[125px]'>
+                            <ChatBubbleLeftRightIcon className='h-4 md:h-6' />
+
+                            Comments
+                        </Link>
+                    }
                 </div>
             }
             
-            <div className='text-amber-300 flex justify-evenly items-center'>
-                <LikeButton logged={loggedData.data?.logged_user?.profile} post={post} />
 
-                <button type='button' className='cursor-pointer flex justify-around items-center 
-                    w-[75px] md:w-[100px] hover:underline' 
-                    onClick={() => sharePost()}>
-                    <ArrowUturnLeftIcon className='h-6' />
-
-                    Share
-                </button>
-
-                <button type='button' className='cursor-pointer flex justify-around items-center 
-                    w-[100px] md:w-[125px] hover:underline' 
-                    onClick={() => toggleTextBox()}>
-                    <ChatBubbleLeftRightIcon className='h-6' />
-
-                    Comment
-                </button>
-            </div>
-
-            {displayTextBox && 
-                <TextBox post={post} poster={loggedData.data.logged_user.profile} cancel_fn={toggleTextBox} />
+            {((post?.commenting_user || post?.commenting_group) && textDisplay) && 
+                <TextBox post={post} poster={loggedData.data?.profile} for_comment={true} cancel_fn={toggleTextBox} />
             }
         </div>
     )
